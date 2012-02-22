28a28a259e1acd266c97ad17f7b07ad3c7b93bd3
Brad Sickles <brad.sickles@gmail.com>
2012-02-22 15:05:04 -0500

    Fixed "Color" property in SolidColorBrush.

--- Source/WickedSick.Fayde.Client.Engine/Javascript/Media/SolidColorBrush.js
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Media/SolidColorBrush.js
@@ -1,13 +1,32 @@
 /// <reference path="../Runtime/RefObject.js" />
 /// <reference path="Brush.js"/>
+/// CODE
+/// <reference path="../Primitives/Color.js"/>
 
 //#region SolidColorBrush
 
 function SolidColorBrush(color) {
     Brush.call(this);
-    this._Color = color;
+    if (!IsDocumentReady())
+        return;
+    if (color != null)
+        this.SetColor(color);
 }
 SolidColorBrush.InheritFrom(Brush);
+
+//#region DEPENDENCY PROPERTIES
+
+SolidColorBrush.ColorProperty = DependencyProperty.Register("Color", function () { return Color; }, SolidColorBrush);
+SolidColorBrush.prototype.GetColor = function () {
+    ///<returns type="Color"></returns>
+    return this.GetValue(SolidColorBrush.ColorProperty);
+};
+SolidColorBrush.prototype.SetColor = function (value) {
+    ///<param name="value" type="Color"></param>
+    this.SetValue(SolidColorBrush.ColorProperty, value);
+};
+
+//#endregion
 
 SolidColorBrush.prototype._Translate = function (ctx) {
     return this._Color.toString();
