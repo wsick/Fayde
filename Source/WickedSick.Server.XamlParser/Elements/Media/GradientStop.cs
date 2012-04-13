63cc6971762ff8211b1ad95aa0477c3c01d3e07e
Brad Sickles <brad.sickles@gmail.com>
2012-04-13 15:13:18 -0400

    Moved GradientStop into correct namespace.

---
+++ Source/WickedSick.Server.XamlParser/Elements/Media/GradientStop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WickedSick.Server.XamlParser.TypeConverters;
+using WickedSick.Server.XamlParser.Elements.Types;
+
+namespace WickedSick.Server.XamlParser.Elements.Media
+{
+    public class GradientStop : DependencyObject
+    {
+        public static readonly PropertyDescription Color = PropertyDescription.Register("Color", typeof(Color), typeof(GradientStop));
+        public static readonly PropertyDescription Offset = PropertyDescription.Register("Offset", typeof(double), typeof(GradientStop));
+    }
+}
