c9e4dff099374fbd6587382de6e3f64ccc2e5885
Brad Sickles <brad.sickles@gmail.com>
2012-04-13 15:48:47 -0400

    Missed Path.js on last check-in.

---
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Shapes/Path.js
@@ -0,0 +1,3 @@
+﻿
+// Path.Data Description: http: //msdn.microsoft.com/en-us/library/system.windows.shapes.path.data(v=vs.95).aspx
+// Path Markup Syntax: http: //msdn.microsoft.com/en-us/library/cc189041(v=vs.95).aspx
