5e246f54c8c1bc3d5a56006e43ef786c5f93930e
BSick7 <brad.sickles@gmail.com>
2012-05-01 19:12:36 -0400

    Fixed package assemblies batch file if contained within directory structure that has a space in it.

--- Source/WickedSick.Fayde.Client.Engine/PackageAssemblies.bat
+++ Source/WickedSick.Fayde.Client.Engine/PackageAssemblies.bat
@@ -3,7 +3,7 @@ SET BD=%~1
 SET progpath="%BD%..\..\JsSingularity.exe"
 IF NOT EXIST %progpath% GOTO NOSINGULARITY
 
-%progpath% %BD% -DeployPath:%BD%Fayde.js -ScriptsFolder:"%BD%Javascript" -BaseIncludesPath:%BD%Javascript -IncludeSubdirectories:true -IncludesFile:%BD%Fayde.order -Debug:true
+%progpath% "%BD%." -DeployPath:"%BD%Fayde.js" -ScriptsFolder:"%BD%Javascript" -BaseIncludesPath:"%BD%Javascript" -IncludeSubdirectories:true -IncludesFile:"%BD%Fayde.order" -Debug:true
 EXIT
 
 :NOSINGULARITY
