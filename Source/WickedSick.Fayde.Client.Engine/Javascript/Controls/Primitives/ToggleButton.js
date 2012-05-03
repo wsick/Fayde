0444e9e39b28ad636d2311d2b30ef83bc1cd231a
ssickles <scottsickles@gmail.com>
2012-05-03 17:47:34 -0400

    - started adding the IsChecked dp and click handling

--- Source/WickedSick.Fayde.Client.Engine/Javascript/Controls/Primitives/ToggleButton.js
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Controls/Primitives/ToggleButton.js
@@ -4,5 +4,25 @@
 //#region ToggleButton
 var ToggleButton = Nullstone.Create("ToggleButton", ButtonBase);
 
+ToggleButton.IsCheckedProperty = DependencyProperty.RegisterReadOnly("IsChecked", function () { return Boolean; }, ToggleButton, false, function (d, args) { d.OnIsCheckedChanged(args); });
+ToggleButton.Instance.GetIsChecked = function () {
+    return this.$GetValue(ToggleButton.IsCheckedProperty);
+};
+ToggleButton.Instance.SetIsChecked = function (value) {
+    this.$SetValue(ToggleButton.IsCheckedProperty, value);
+};
+
+ToggleButton.Instance.OnIsCheckedChanged = function (e) {
+    var isChecked = e.NewValue;
+    //TODO: add the code for raising the appropriate routed event
+    //      OnChecked, OnUnchecked, OnIndeterminate
+    this.UpdateVisualState();
+};
+
+ToggleButton.Instance.OnClick = function () {
+    alert('clicked');
+    this.SetIsChecked(true);
+};
+
 Nullstone.FinishCreate(ToggleButton);
 //#endregion
