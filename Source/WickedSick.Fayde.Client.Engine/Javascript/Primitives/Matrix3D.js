a44f63404d4e56b4cffb86fc0334118c4c212489
Brad Sickles <brad.sickles@gmail.com>
2012-06-12 13:55:22 -0400

    Added inverse calculation. Changed Matrix3D.TransformPoint and Matrix3D.Multiply to not overwrite in-mem values if c & b or C & B were the same in-mem object.

--- Source/WickedSick.Fayde.Client.Engine/Javascript/Primitives/Matrix3D.js
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Primitives/Matrix3D.js
@@ -12,6 +12,16 @@ function Matrix3D() {
         0, 0, 1, 0,
         0, 0, 0, 1
     ];
+    Object.defineProperty(this, "Inverse", {
+        get: function () {
+            if (!this._InverseEls)
+                this._InverseEls = Matrix3D._CalculateInverse(this);
+            var m3 = new Matrix3D();
+            m3._Elements = this._InverseEls;
+            m3._InverseEls = this._Elements;
+            return m3;
+        }
+    });
 }
 Matrix3D.prototype.toString = function () {
     return this._Elements.toString();
@@ -70,11 +80,18 @@ Matrix3D.TransformPoint = function (c, A, b) {
     /// <param name="c" type="Array">[x, y, z, w]</param>
     /// <param name="A" type="Matrix3D"></param>
     /// <param name="b" type="Array">[x, y, z, w]</param>
-    var e = A[0];
-    c[0] = e[0] * b[0] + e[1] * b[1] + e[2] * b[2] + e[3];
-    c[1] = e[4] * b[0] + e[5] * b[1] + e[6] * b[2] + e[7];
-    c[2] = e[8] * b[0] + e[9] * b[1] + e[10] * b[2] + e[11];
-    c[3] = e[12] * b[0] + e[13] * b[1] + e[14] * b[2] + e[15];
+    var e = A._Elements;
+    var d = [0, 0, 0, 1];
+    //if user passed in same point for c and b, we don't want to overwrite values as we're using them
+    d[0] = e[0] * b[0] + e[1] * b[1] + e[2] * b[2] + e[3];
+    d[1] = e[4] * b[0] + e[5] * b[1] + e[6] * b[2] + e[7];
+    d[2] = e[8] * b[0] + e[9] * b[1] + e[10] * b[2] + e[11];
+    d[3] = e[12] * b[0] + e[13] * b[1] + e[14] * b[2] + e[15];
+
+    c[0] = d[0];
+    c[1] = d[1];
+    c[2] = d[2];
+    c[3] = d[3];
 };
 Matrix3D.Equals = function (A, B) {
     /// <summary>Performs equality test on all items in A & B.</summary>
@@ -99,4 +116,51 @@ Matrix3D.Get2DAffine = function (A) {
         mt._Type = MatrixTypes.Unknown;
         return mt;
     }
+};
+Matrix3D._CalculateInverse = function (m) {
+    var els = m._Elements;
+    var tmp = [];
+
+    tmp[0] = els[5] * els[10] * els[15] - els[5] * els[14] * els[11] - els[6] * els[9] * els[15] +
+		els[6] * els[13] * els[11] + els[7] * els[9] * els[14] - els[7] * els[13] * els[10];
+    tmp[1] = -els[1] * els[10] * els[15] + els[1] * els[14] * els[11] + els[2] * els[9] * els[15] -
+		els[2] * els[13] * els[11] - els[3] * els[9] * els[14] + els[3] * els[13] * els[10];
+    tmp[2] = els[1] * els[6] * els[15] - els[1] * els[14] * els[7] - els[2] * els[5] * els[15] +
+		els[2] * els[13] * els[7] + els[3] * els[5] * els[14] - els[3] * els[13] * els[6];
+    tmp[3] = -els[1] * els[6] * els[11] + els[1] * els[10] * els[7] + els[2] * els[5] * els[11] -
+		els[2] * els[9] * els[7] - els[3] * els[5] * els[10] + els[3] * els[9] * els[6];
+    tmp[4] = -els[4] * els[10] * els[15] + els[4] * els[14] * els[11] + els[6] * els[8] * els[15] -
+		els[6] * els[12] * els[11] - els[7] * els[8] * els[14] + els[7] * els[12] * els[10];
+    tmp[5] = els[0] * els[10] * els[15] - els[0] * els[14] * els[11] - els[2] * els[8] * els[15] +
+		els[2] * els[12] * els[11] + els[3] * els[8] * els[14] - els[3] * els[12] * els[10];
+    tmp[6] = -els[0] * els[6] * els[15] + els[0] * els[14] * els[7] + els[2] * els[4] * els[15] -
+		els[2] * els[12] * els[7] - els[3] * els[4] * els[14] + els[3] * els[12] * els[6];
+    tmp[7] = els[0] * els[6] * els[11] - els[0] * els[10] * els[7] - els[2] * els[4] * els[11] +
+		els[2] * els[8] * els[7] + els[3] * els[4] * els[10] - els[3] * els[8] * els[6];
+    tmp[8] = els[4] * els[9] * els[15] - els[4] * els[13] * els[11] - els[5] * els[8] * els[15] +
+		els[5] * els[12] * els[11] + els[7] * els[8] * els[13] - els[7] * els[12] * els[9];
+    tmp[9] = -els[0] * els[9] * els[15] + els[0] * els[13] * els[11] + els[1] * els[8] * els[15] -
+		els[1] * els[12] * els[11] - els[3] * els[8] * els[13] + els[3] * els[12] * els[9];
+    tmp[10] = els[0] * els[5] * els[15] - els[0] * els[13] * els[7] - els[1] * els[4] * els[15] +
+		els[1] * els[12] * els[7] + els[3] * els[4] * els[13] - els[3] * els[12] * els[5];
+    tmp[11] = -els[0] * els[5] * els[11] + els[0] * els[9] * els[7] + els[1] * els[4] * els[11] -
+		els[1] * els[8] * els[7] - els[3] * els[4] * els[9] + els[3] * els[8] * els[5];
+    tmp[12] = -els[4] * els[9] * els[14] + els[4] * els[13] * els[10] + els[5] * els[8] * els[14] -
+		els[5] * els[12] * els[10] - els[6] * els[8] * els[13] + els[6] * els[12] * els[9];
+    tmp[13] = els[0] * els[9] * els[14] - els[0] * els[13] * els[10] - els[1] * els[8] * els[14] +
+		els[1] * els[12] * els[10] + els[2] * els[8] * els[13] - els[2] * els[12] * els[9];
+    tmp[14] = -els[0] * els[5] * els[14] + els[0] * els[13] * els[6] + els[1] * els[4] * els[14] -
+		els[1] * els[12] * els[6] - els[2] * els[4] * els[13] + els[2] * els[12] * els[5];
+    tmp[15] = els[0] * els[5] * els[10] - els[0] * els[9] * els[6] - els[1] * els[4] * els[10] +
+		els[1] * els[8] * els[6] + els[2] * els[4] * els[9] - els[2] * els[8] * els[5];
+
+    var det = els[0] * tmp[0] + els[4] * tmp[1] + els[8] * tmp[2] + els[12] * tmp[3];
+    if (det === 0)
+        return;
+    det = 1.0 / det;
+    var inverse = [];
+    for (var i = 0; i < 16; i++) {
+        inverse.push(tmp[i] * det);
+    }
+    return inverse;
 };
