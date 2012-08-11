9d94d350ad605df4233ad6c2065ee98d8dad1476
BSick7 <brad.sickles@gmail.com>
2012-08-11 10:17:32 -0400

    Made it possible to declare a Double in xaml.

---
+++ Source/WickedSick.Server.XamlParser/Elements/Core/Double.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WickedSick.Server.XamlParser.Elements.Core
+{
+    public class Double : DependencyObject
+    {
+        public static readonly PropertyDescription Content = PropertyDescription.Register("Content", typeof(string), typeof(Double), true);
+
+        public override string ToJson(int tabIndent)
+        {
+            return GetValue("Content") as string;
+        }
+    }
+}
