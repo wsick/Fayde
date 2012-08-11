445a68e9f18e91bd5a925110d87c5dfa0854cc4d
BSick7 <brad.sickles@gmail.com>
2012-08-11 15:30:53 -0400

    Added ComboBoxItem stub.

---
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Controls/ComboBoxItem.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="ListBoxItem.js"/>
+/// CODE
+
+//#region ComboBoxItem
+var ComboBoxItem = Nullstone.Create("ComboBoxItem", ListBoxItem);
+
+
+
+Nullstone.FinishCreate(ComboBoxItem);
+//#endregion
