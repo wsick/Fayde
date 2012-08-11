275fd653089dd3ce47fe3655b94be4e8033c03f8
BSick7 <brad.sickles@gmail.com>
2012-08-11 11:11:34 -0400

    Added Style.BasedOn.

--- Source/WickedSick.Server.XamlParser/Elements/Style.cs
+++ Source/WickedSick.Server.XamlParser/Elements/Style.cs
@@ -6,5 +6,6 @@ namespace WickedSick.Server.XamlParser.Elements
     {
         public static readonly PropertyDescription Setters = PropertyDescription.Register("Setters", typeof(DependencyObjectCollection<Setter>), typeof(Style), true);
         public static readonly PropertyDescription TargetType = PropertyDescription.Register("TargetType", typeof(JsType), typeof(Style));
+        public static readonly PropertyDescription BasedOnProperty = PropertyDescription.Register("BasedOn", typeof(Style), typeof(Style));
     }
 }
