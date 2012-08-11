372828520fe52f461d58139fd1257a79398d9748
BSick7 <brad.sickles@gmail.com>
2012-08-11 11:05:33 -0400

    Added UIElement.Effect + BlurEffect stub.

---
+++ Source/WickedSick.Server.XamlParser/Elements/Media/Effects/BlurEffect.cs
@@ -0,0 +1,8 @@
+﻿
+namespace WickedSick.Server.XamlParser.Elements.Media.Effects
+{
+    public class BlurEffect : Effect
+    {
+        public static readonly PropertyDescription RadiusProperty = PropertyDescription.Register("Radius", typeof(double), typeof(BlurEffect));
+    }
+}
