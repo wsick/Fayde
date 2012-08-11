6bd16270f7804ed52cf9bb0a0b7756c079668888
BSick7 <brad.sickles@gmail.com>
2012-08-11 10:44:51 -0400

    Added basic animation stubs.

---
+++ Source/WickedSick.Server.XamlParser/Elements/Media/Animation/DoubleKeyFrame.cs
@@ -0,0 +1,10 @@
+﻿using WickedSick.Server.XamlParser.Elements.Types;
+
+namespace WickedSick.Server.XamlParser.Elements.Media.Animation
+{
+    public class DoubleKeyFrame : DependencyObject
+    {
+        public static readonly PropertyDescription KeyTimeProperty = PropertyDescription.Register("KeyTime", typeof(KeyTime), typeof(DoubleKeyFrame));
+        public static readonly PropertyDescription ValueProperty = PropertyDescription.Register("Value", typeof(double), typeof(DoubleKeyFrame));
+    }
+}
---
+++ Source/WickedSick.Server.XamlParser/Elements/Media/Animation/SplineDoubleKeyFrame.cs
@@ -0,0 +1,9 @@
+﻿
+namespace WickedSick.Server.XamlParser.Elements.Media.Animation
+{
+    public class SplineDoubleKeyFrame : DoubleKeyFrame
+    {
+        public static readonly PropertyDescription KeySplineProperty = PropertyDescription.Register("KeySpline", typeof(KeySpline), typeof(SplineDoubleKeyFrame));
+
+    }
+}
