7fe08708d22771442244081c142391fd6f1b3ea0
BSick7 <brad.sickles@gmail.com>
2012-08-11 14:51:58 -0400

    Added EasingColorKeyFrame stub.

---
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Media/Animation/EasingColorKeyFrame.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="ColorKeyFrame.js"/>
+/// CODE
+/// <reference path="IEasingFunction.js"/>
+
+//#region EasingColorKeyFrame
+var EasingColorKeyFrame = Nullstone.Create("EasingColorKeyFrame", ColorKeyFrame);
+
+//#region Properties
+
+EasingColorKeyFrame.EasingFunctionProperty = DependencyProperty.Register("EasingFunction", function () { return IEasingFunction; }, EasingColorKeyFrame);
+
+Nullstone.AutoProperties(EasingColorKeyFrame, [
+    EasingColorKeyFrame.EasingFunctionProperty
+]);
+
+//#endregion
+
+Nullstone.FinishCreate(EasingColorKeyFrame);
+//#endregion
