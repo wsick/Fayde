96d205cdc4787b20140ff4a207be3a93504fc3d3
BSick7 <brad.sickles@gmail.com>
2012-08-11 10:18:57 -0400

    Fixed ContentControl. Not an abstract class.

--- Source/WickedSick.Server.XamlParser/Elements/Controls/ContentControl.cs
+++ Source/WickedSick.Server.XamlParser/Elements/Controls/ContentControl.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace WickedSick.Server.XamlParser.Elements.Controls
 {
-    public abstract class ContentControl : Control
+    public class ContentControl : Control
     {
         public static readonly PropertyDescription Content = PropertyDescription.Register("Content", typeof(object), typeof(ContentControl), true);
     }
