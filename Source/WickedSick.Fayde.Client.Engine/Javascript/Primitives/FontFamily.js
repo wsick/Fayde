06bfe35b52735f0c4bc1cb774ca38932e65ae0ed
BSick7 <brad.sickles@gmail.com>
2012-08-11 11:32:53 -0400

    Fixed dependencies for FontFamily.

--- Source/WickedSick.Fayde.Client.Engine/Javascript/Primitives/FontFamily.js
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Primitives/FontFamily.js
@@ -1,4 +1,7 @@
-﻿//#region FontFamily
+﻿/// <reference path="../Runtime/Nullstone.js"/>
+/// CODE
+
+//#region FontFamily
 var FontFamily = Nullstone.Create("FontFamily");
 
 FontFamily.Instance.Init = function (familyNames) {
