9df60c62c189529d0f29458c4cdab4fd6a7272c2
BSick7 <brad.sickles@gmail.com>
2012-08-16 12:24:30 -0400

    Fixed FontWeights for text.

--- Source/WickedSick.Fayde.Client.Engine/Javascript/Primitives/Enums.js
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Primitives/Enums.js
@@ -55,10 +55,16 @@ var LineStackingStrategy = {
 };
 
 var FontWeight = {
-    Normal: "normal",
-    Bold: "bold",
-    Bolder: "bolder",
-    Lighter: "lighter"
+    Thin: 100,
+    ExtraLight: 200,
+    Light: 300,
+    Normal: 400,
+    Medium: 500,
+    SemiBold: 600,
+    Bold: 700,
+    ExtraBold: 800,
+    Black: 900,
+    ExtraBlack: 950
 };
 
 var FontStyle = {
