a57738cc5c6670ccfec900127faf47d95325e94c
BSick7 <brad.sickles@gmail.com>
2012-08-16 14:37:34 -0400

    Changed ProgressBar and Slider stubs to inherit from RangeBase.

--- Source/WickedSick.Fayde.Client.Engine/Javascript/Controls/ProgressBar.js
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Controls/ProgressBar.js
@@ -1,8 +1,8 @@
-﻿/// <reference path="Control.js"/>
+﻿/// <reference path="Primitives/RangeBase.js"/>
 /// CODE
 
 //#region ProgressBar
-var ProgressBar = Nullstone.Create("ProgressBar", Control);
+var ProgressBar = Nullstone.Create("ProgressBar", RangeBase);
 
 ProgressBar.Instance.Init = function () {
     this.Init$Control();
--- Source/WickedSick.Fayde.Client.Engine/Javascript/Controls/Slider.js
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Controls/Slider.js
@@ -1,8 +1,8 @@
-﻿/// <reference path="Control.js"/>
+﻿/// <reference path="Primitives/RangeBase.js"/>
 /// CODE
 
 //#region Slider
-var Slider = Nullstone.Create("Slider", Control);
+var Slider = Nullstone.Create("Slider", RangeBase);
 
 Slider.Instance.Init = function () {
     this.Init$Control();
