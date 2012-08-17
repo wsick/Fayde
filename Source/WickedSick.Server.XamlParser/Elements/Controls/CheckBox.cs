89ad4c085b74dfea3783cfed8663ee3380f6094a
BSick7 <brad.sickles@gmail.com>
2012-08-17 17:35:04 -0400

    Fixed ToggleButton definition.

--- Source/WickedSick.Server.XamlParser/Elements/Controls/CheckBox.cs
+++ Source/WickedSick.Server.XamlParser/Elements/Controls/CheckBox.cs
@@ -4,7 +4,5 @@ namespace WickedSick.Server.XamlParser.Elements.Controls
 {
     public class CheckBox : ToggleButton
     {
-        public static readonly PropertyDescription IsCheckedProperty = PropertyDescription.Register("IsChecked", typeof(Core.Boolean), typeof(CheckBox));
-        public static readonly PropertyDescription IsIndeterminateProperty = PropertyDescription.Register("IsIndeterminate", typeof(bool), typeof(CheckBox));
     }
 }
