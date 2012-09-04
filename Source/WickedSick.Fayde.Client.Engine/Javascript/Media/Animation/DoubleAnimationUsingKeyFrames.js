31ce05f11a5adeb35fb419647708b863137d3428
BSick7 <brad.sickles@gmail.com>
2012-09-04 12:58:05 -0400

    Changed KeyFrame animations to inherit from AnimationUsingKeyFrames.

--- Source/WickedSick.Fayde.Client.Engine/Javascript/Media/Animation/DoubleAnimationUsingKeyFrames.js
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Media/Animation/DoubleAnimationUsingKeyFrames.js
@@ -1,12 +1,12 @@
-﻿/// <reference path="Animation.js"/>
+﻿/// <reference path="AnimationUsingKeyFrames.js"/>
 /// <reference path="DoubleKeyFrameCollection.js"/>
 /// CODE
 /// <reference path="DoubleKeyFrame.js"/>
 
 //#region DoubleAnimationUsingKeyFrames
-var DoubleAnimationUsingKeyFrames = Nullstone.Create("DoubleAnimationUsingKeyFrames", Animation);
+var DoubleAnimationUsingKeyFrames = Nullstone.Create("DoubleAnimationUsingKeyFrames", AnimationUsingKeyFrames);
 
-//#region Dependency Properties
+//#region Properties
 
 DoubleAnimationUsingKeyFrames.KeyFramesProperty = DependencyProperty.RegisterFull("KeyFrames", function () { return DoubleKeyFrameCollection; }, DoubleAnimationUsingKeyFrames, undefined, { GetValue: function () { return new DoubleKeyFrameCollection(); } });
 
@@ -15,65 +15,6 @@ Nullstone.AutoProperties(DoubleAnimationUsingKeyFrames, [
 ]);
 
 //#endregion
-
-DoubleAnimationUsingKeyFrames.Instance.Resolve = function () {
-    var keyFrames = this.KeyFrames;
-
-    KeyFrameCollection.ResolveKeyFrames(this, keyFrames);
-
-    var count = keyFrames._SortedList.length;
-    for (var j = 0; j < count; j++) {
-        if (!keyFrames._SortedList[j].KeyTime.IsValid)
-            return false;
-    }
-
-    return true;
-};
-
-DoubleAnimationUsingKeyFrames.Instance._GetCurrentValue = function (defaultOriginValue, defaultDestinationValue, clockData) {
-    var keyFrames = this.KeyFrames;
-
-    var prevFrameRef = {};
-    var currentKeyFrame = keyFrames.GetKeyFrameForTime(clockData.CurrentTime, prevFrameRef);
-    var prevFrame = prevFrameRef.Value;
-    if (currentKeyFrame == null)
-        return null;
-
-    var baseValue;
-    var keyStartTime;
-    var keyEndTime = currentKeyFrame._ResolvedKeyTime;
-    if (prevFrame == null) {
-        // the first keyframe, start at the animation's base value
-        baseValue = defaultOriginValue;
-        keyStartTime = new TimeSpan();
-    } else {
-        // start at the previous keyframe's target value
-        baseValue = prevFrame.ConvertedValue;
-        keyStartTime = prevFrame._ResolvedKeyTime;
-    }
-
-    var progress;
-    if (clockData.CurrentTime.CompareTo(keyEndTime) >= 0) {
-        progress = 1.0;
-    } else {
-        var keyDuration = keyEndTime._Ticks - keyStartTime._Ticks;
-        if (keyDuration <= 0)
-            progress = 1.0;
-        else
-            progress = (clockData.CurrentTime._Ticks - keyStartTime._Ticks) / keyDuration;
-    }
-
-    return currentKeyFrame.InterpolateValue(baseValue, progress);
-};
-
-DoubleAnimationUsingKeyFrames.Instance.AddKeyFrame = function (frame) {
-    /// <param name="frame" type="DoubleKeyFrame"></param>
-    this.KeyFrames.Add(frame);
-};
-DoubleAnimationUsingKeyFrames.Instance.RemoveKeyFrame = function (frame) {
-    /// <param name="frame" type="DoubleKeyFrame"></param>
-    this.KeyFrames.Remove(frame);
-};
 
 //#region Annotations
 
