5a5006c1f77647b56dba0b474b43b1f8e0bb199d
BSick7 <brad.sickles@gmail.com>
2012-09-06 13:53:45 -0400

    Finished implementing ListBoxItem.

--- Source/WickedSick.Fayde.Client.Engine/Javascript/Controls/ListBoxItem.js
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Controls/ListBoxItem.js
@@ -4,33 +4,33 @@
 //#region ListBox
 var ListBoxItem = Nullstone.Create("ListBoxItem", ContentControl);
 
+ListBoxItem.Instance.Init = function () {
+    this.Init$ContentControl();
+    this._ParentSelectorChanged = new MulticastEvent();
+    this.DefaultStyleKey = this.constructor;
+};
+
 //#region Properties
 
 ListBoxItem.IsSelectedProperty = DependencyProperty.RegisterCore("IsSelected", function () { return Boolean; }, ListBoxItem, null, function (d, args) { d.OnIsSelectedChanged(args); });
 
 Nullstone.AutoProperties(ListBoxItem, [
-    ListBoxItem.IsSelectedProperty]);
+    ListBoxItem.IsSelectedProperty
+]);
 
 Nullstone.Property(ListBoxItem, "ParentSelector", {
     get: function () {
-        return this._parentSelector;
+        return this._ParentSelector;
     },
-    set: function () {
-        if (this._parentSelector == value) {
+    set: function (value) {
+        if (Nullstone.RefEquals(this._ParentSelector, value))
             return;
-        }
-        this._parentSelector = value;
-        this._parentSelectorChanged.Raise(this, null);
+        this._ParentSelector = value;
+        this._ParentSelectorChanged.Raise(this, new EventArgs());
     }
 });
 
 //#endregion
-
-ListBoxItem.Instance.Init = function () {
-    this.Init$ContentControl();
-    this._parentSelectorChanged = new MulticastEvent();
-    this.DefaultStyleKey = this.constructor;
-};
 
 ListBoxItem.Instance.OnApplyTemplate = function () {
     this.OnApplyTemplate$ContentControl();
@@ -43,35 +43,49 @@ ListBoxItem.Instance.OnIsSelectedChanged = function (e) {
 
 ListBoxItem.Instance.InvokeLoaded = function () {
     this.InvokeLoaded$ContentControl();
-    if (this._parentSelector) {
-        this._parentSelector.NotifyListItemLoaded(this);
+    if (this._ParentSelector != null) {
+        this._ParentSelector.NotifyListItemLoaded(this);
     }
 };
 
+//#region Mouse/Focus Handling
+
 ListBoxItem.Instance.OnMouseLeftButtonDown = function (sender, args) {
-    if (!args.Handled) {
-        args.Handled = true;
-        if (this.Focus(true)) {
-            if (this._parentSelector) {
-                this._parentSelector.NotifyListItemClicked(this);
-            }
-        }
+    if (args.Handled)
+        return;
+    args.Handled = true;
+    if (!this.Focus(true))
+        return;
+    if (this._ParentSelector != null) {
+        this._ParentSelector.NotifyListItemClicked(this);
     }
+};
+ListBoxItem.Instance.OnMouseEnter = function () {
+    this.OnMouseEnter$ContentControl();
+    this.$UpdateVisualState();
+};
+ListBoxItem.Instance.OnMouseLeave = function () {
+    this.OnMouseLeave$ContentControl();
+    this.$UpdateVisualState();
 };
 ListBoxItem.Instance.OnGotFocus = function (sender, args) {
     this.OnGotFocus$ContentControl();
     this.$UpdateVisualState();
-    if (this._parentSelector) {
-        this._parentSelector.NotifyListItemGotFocus(this);
+    if (this._ParentSelector != null) {
+        this._ParentSelector.NotifyListItemGotFocus(this);
     }
 };
 ListBoxItem.Instance.OnLostFocus = function (sender, args) {
     this.OnLostFocus$ContentControl();
     this.$UpdateVisualState();
-    if (this._parentSelector) {
-        this._parentSelector.NotifyListItemLostFocus(this);
+    if (this._ParentSelector != null) {
+        this._ParentSelector.NotifyListItemLostFocus(this);
     }
 };
+
+//#endregion
+
+//#region Visual State
 
 ListBoxItem.Instance.$GetVisualStateNamesToActivate = function () {
     var arr = this.$GetVisualStateNamesToActivate$ContentControl();
@@ -94,6 +108,8 @@ ListBoxItem.Instance.$GetVisualStateSelection = function () {
         return "Unselected";
     }
 };
+
+//#endregion
 
 Nullstone.FinishCreate(ListBoxItem);
 //#endregion
