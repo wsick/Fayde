71a77035a4ea026e745a4a6a590fe10743eb8379
BSick7 <brad.sickles@gmail.com>
2012-09-07 15:14:31 -0400

    Added Array and String that can be embedded xaml.

---
+++ Source/WickedSick.Server.XamlParser/Elements/Core/Array.cs
@@ -0,0 +1,8 @@
+﻿
+namespace WickedSick.Server.XamlParser.Elements.Core
+{
+    public class Array : DependencyObjectCollection<object>
+    {
+        public static readonly PropertyDescription ElementsProperty = PropertyDescription.Register("Elements", typeof(DependencyObjectCollection<object>), typeof(Array), true);
+    }
+}
---
+++ Source/WickedSick.Server.XamlParser/Elements/Core/String.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WickedSick.Server.XamlParser.Elements.Core
+{
+    public class String : DependencyObject
+    {
+        public static readonly PropertyDescription ValueProperty = PropertyDescription.Register("Value", typeof(string), typeof(String), true);
+
+        public override string ToJson(int tabIndent)
+        {
+            return string.Format("\"{0}\"", GetValue("Value"));
+        }
+    }
+}
