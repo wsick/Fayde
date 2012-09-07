150526beea3363949b9e06ae70e091f054ce05fd
BSick7 <brad.sickles@gmail.com>
2012-09-07 11:35:17 -0400

    Added interface definition to INotifyCollectionChanged.

--- Source/WickedSick.Fayde.Client.Engine/Javascript/Collections/INotifyCollectionChanged.js
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Collections/INotifyCollectionChanged.js
@@ -4,5 +4,9 @@
 //#region INotifyCollectionChanged
 var INotifyCollectionChanged = Nullstone.Create("INotifyCollectionChanged");
 
+INotifyCollectionChanged.Instance.Init = function () {
+    this.CollectionChanged = new MulticastEvent();
+};
+
 Nullstone.FinishCreate(INotifyCollectionChanged);
 //#endregion
