b546220a1a630dfdc03fbddeddff3b23e5736401
BSick7 <brad.sickles@gmail.com>
2012-09-07 14:59:02 -0400

    Added ItemsControl properties to support ItemsSource.

--- Source/WickedSick.Server.XamlParser/Elements/Controls/ItemsControl.cs
+++ Source/WickedSick.Server.XamlParser/Elements/Controls/ItemsControl.cs
@@ -8,5 +8,7 @@ namespace WickedSick.Server.XamlParser.Elements.Controls
     public class ItemsControl: Control
     {
         public static readonly PropertyDescription Items = PropertyDescription.Register("Items", typeof(DependencyObjectCollection<object>), typeof(ItemsControl), true);
+        public static readonly PropertyDescription ItemTemplateProperty = PropertyDescription.Register("ItemTemplate", typeof(DataTemplate), typeof(ItemsControl));
+        public static readonly PropertyDescription ItemsSourceProperty = PropertyDescription.Register("ItemsSource", typeof(DependencyObjectCollection<object>), typeof(ItemsControl));
     }
 }
