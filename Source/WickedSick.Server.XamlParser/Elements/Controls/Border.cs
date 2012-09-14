a12995b4f8c0198788c36d84b23007ae514db3f5
BSick7 <brad.sickles@gmail.com>
2012-09-14 11:15:00 -0400

    Missing Border.Padding.

--- Source/WickedSick.Server.XamlParser/Elements/Controls/Border.cs
+++ Source/WickedSick.Server.XamlParser/Elements/Controls/Border.cs
@@ -10,10 +10,12 @@ namespace WickedSick.Server.XamlParser.Elements.Controls
 {
     public class Border : FrameworkElement
     {
-        public static readonly PropertyDescription Background = PropertyDescription.Register("Background", typeof(Brush), typeof(Border));
-        public static readonly PropertyDescription BorderBrush = PropertyDescription.Register("BorderBrush", typeof(Brush), typeof(Border));
-        public static readonly PropertyDescription BorderThickness = PropertyDescription.Register("BorderThickness", typeof(Thickness), typeof(Border));
-        public static readonly PropertyDescription CornerRadius = PropertyDescription.Register("CornerRadius", typeof(CornerRadius), typeof(Border));
-        public static readonly PropertyDescription Child = PropertyDescription.Register("Child", typeof(UIElement), typeof(Border), true);
+        public static readonly PropertyDescription ChildProperty = PropertyDescription.Register("Child", typeof(UIElement), typeof(Border), true);
+
+        public static readonly PropertyDescription BackgroundProperty = PropertyDescription.Register("Background", typeof(Brush), typeof(Border));
+        public static readonly PropertyDescription BorderBrushProperty = PropertyDescription.Register("BorderBrush", typeof(Brush), typeof(Border));
+        public static readonly PropertyDescription BorderThicknessProperty = PropertyDescription.Register("BorderThickness", typeof(Thickness), typeof(Border));
+        public static readonly PropertyDescription CornerRadiusProperty = PropertyDescription.Register("CornerRadius", typeof(CornerRadius), typeof(Border));
+        public static readonly PropertyDescription PaddingProperty = PropertyDescription.Register("Padding", typeof(Thickness), typeof(Border));
     }
 }
