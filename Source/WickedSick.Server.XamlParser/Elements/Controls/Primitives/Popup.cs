83852c9618dde71c830ffa73127bf8e9a36c9727
BSick7 <brad.sickles@gmail.com>
2012-09-24 14:04:03 -0400

    Added Popup DPs to server-side.

--- Source/WickedSick.Server.XamlParser/Elements/Controls/Primitives/Popup.cs
+++ Source/WickedSick.Server.XamlParser/Elements/Controls/Primitives/Popup.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace WickedSick.Server.XamlParser.Elements.Controls.Primitives
 {
-    public class Popup: FrameworkElement
+    public class Popup : FrameworkElement
     {
-        public static readonly PropertyDescription Child = PropertyDescription.Register("Child", typeof(UIElement), typeof(Popup), true);
+        public static readonly PropertyDescription ChildProperty = PropertyDescription.Register("Child", typeof(UIElement), typeof(Popup), true);
+        public static readonly PropertyDescription IsOpenProperty = PropertyDescription.Register("IsOpen", typeof(bool), typeof(Popup));
+        public static readonly PropertyDescription HorizontalOffsetProperty = PropertyDescription.Register("HorizontalOffset", typeof(double), typeof(Popup));
+        public static readonly PropertyDescription VerticalOffsetProperty = PropertyDescription.Register("VerticalOffset", typeof(double), typeof(Popup));
     }
 }
