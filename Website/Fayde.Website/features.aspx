c27f47d249e5989ff930c0c57bf1670b35475142
BSick7 <brad.sickles@gmail.com>
2012-09-28 19:53:23 -0400

    Implemented features page.

--- Website/Fayde.Website/features.aspx
+++ Website/Fayde.Website/features.aspx
@@ -1,6 +1,44 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/default.Master" AutoEventWireup="true" CodeBehind="features.aspx.cs" Inherits="Fayde.Website.features" Theme="default" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <script type="text/javascript">
+        function toggle(li) {
+            if (!li.isopen) {
+                li.isopen = true;
+                li.className = li.className.replace("collapsed", "expanded");
+            } else {
+                li.isopen = false;
+                li.className = li.className.replace("expanded", "collapsed");
+            }
+        }
+    </script>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="cphBody" runat="server">
-    Coming Soon!
+    <p>
+        The following is an up-to-date status of implementation of individual components.
+    </p>
+    <p>
+        Legend:
+    </p>
+    <ul>
+        <li>Full <asp:Image ImageUrl="~/App_Themes/default/images/full.png" runat="server" /></li>
+        <li>Partial <asp:Image ID="Image1" ImageUrl="~/App_Themes/default/images/partial.png" runat="server" /></li>
+        <li>None <asp:Image ID="Image2" ImageUrl="~/App_Themes/default/images/none.png" runat="server" /></li>
+    </ul>
+    <asp:Repeater ID="rptFeatures" runat="server" OnItemDataBound="rptFeatures_ItemDataBound">
+        <HeaderTemplate>
+            <ul>
+        </HeaderTemplate>
+        <ItemTemplate>
+            <li class="collapsed <%# GetListItemClass(Container.DataItem) %>">
+                <span class="<%# GetNameClass(Container.DataItem) %>" onclick="toggle(this.parentNode);"><%# Eval("Name") %></span>
+                &nbsp;&nbsp;&nbsp;<img src="<%# GetSupportLevelImgSrc(Container.DataItem) %>" />
+                <div id="subtree">
+                    <asp:PlaceHolder ID="phSubtree" runat="server" />
+                </div>
+            </li>
+        </ItemTemplate>
+        <FooterTemplate>
+            </ul>
+        </FooterTemplate>
+    </asp:Repeater>
 </asp:Content>
--- Website/Fayde.Website/features.aspx.cs
+++ Website/Fayde.Website/features.aspx.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
+using Fayde.Website.Models;
 
 namespace Fayde.Website
 {
@@ -11,7 +9,74 @@ namespace Fayde.Website
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+                BindData();
+        }
 
+        private void BindData()
+        {
+            rptFeatures.DataSource = Fayde.Website.IO.FeaturesXmlLoader.Load(Server.MapPath("~/App_Data/features_list.xml"));
+            rptFeatures.DataBind();
+        }
+
+        protected void rptFeatures_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType != ListItemType.AlternatingItem && e.Item.ItemType != ListItemType.Item)
+                return;
+            var feature = e.Item.DataItem as Feature;
+            if (!feature.Features.Any())
+                return;
+            var phSubtree = e.Item.FindControl("phSubtree");
+            var rpt = new Repeater();
+            rpt.DataSource = feature.Features; 
+            rpt.HeaderTemplate = (sender as Repeater).HeaderTemplate;
+            rpt.FooterTemplate = (sender as Repeater).FooterTemplate;
+            rpt.ItemTemplate = (sender as Repeater).ItemTemplate;
+            rpt.ItemDataBound += rptFeatures_ItemDataBound;
+            phSubtree.Controls.Add(rpt);
+            rpt.DataBind();
+        }
+
+        protected string GetListItemClass(object dataItem)
+        {
+            var feature = dataItem as Feature;
+            switch (feature.Type)
+            {
+                case FeatureTypes.Namespace:
+                    return "feature_namespace";
+                case FeatureTypes.Class:
+                    return "feature_class";
+                case FeatureTypes.AttachedDependencyProperty:
+                case FeatureTypes.DependencyProperty:
+                case FeatureTypes.Property:
+                    return "feature_property";
+                case FeatureTypes.Method:
+                    return "feature_method";
+                case FeatureTypes.Event:
+                    return "feature_event";
+            }
+            return null;
+        }
+
+        protected string GetNameClass(object dataItem)
+        {
+            var feature = dataItem as Feature;
+            return feature.Features.Any() ? "feature_has_children" : null;
+        }
+
+        protected string GetSupportLevelImgSrc(object dataItem)
+        {
+            var feature = dataItem as Feature;
+            switch (feature.SupportLevel)
+            {
+                case SupportLevels.Full:
+                    return "App_Themes/default/images/full.png";
+                case SupportLevels.Partial:
+                    return "App_Themes/default/images/partial.png";
+                case SupportLevels.None:
+                    return "App_Themes/default/images/none.png";
+            }
+            return null;
         }
     }
 }
