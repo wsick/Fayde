7da3c2d6e10fa8d735d1b5491f120eb370c37903
BSick7 <brad.sickles@gmail.com>
2012-09-29 11:38:45 -0400

    Added all Linear Key Frames.

---
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Media/Animation/LinearColorKeyFrame.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="ColorKeyFrame.js"/>
+/// CODE
+/// <reference path="../../Primitives/Color.js"/>
+
+//#region LinearColorKeyFrame
+var LinearColorKeyFrame = Nullstone.Create("LinearColorKeyFrame", ColorKeyFrame);
+
+LinearColorKeyFrame.Instance.InterpolateValue = function (baseValue, keyFrameProgress) {
+    return Color.LERP(baseValue, this.Value, keyFrameProgress);
+};
+
+Nullstone.FinishCreate(LinearColorKeyFrame);
+//#endregion
---
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Media/Animation/LinearPointKeyFrame.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="PointKeyFrame.js"/>
+/// CODE
+/// <reference path="../../Primitives/Point.js"/>
+
+//#region LinearPointKeyFrame
+var LinearPointKeyFrame = Nullstone.Create("LinearPointKeyFrame", PointKeyFrame);
+
+LinearPointKeyFrame.Instance.InterpolateValue = function (baseValue, keyFrameProgress) {
+    return Point.LERP(baseValue, this.Value, keyFrameProgress);
+};
+
+Nullstone.FinishCreate(LinearPointKeyFrame);
+//#endregion
