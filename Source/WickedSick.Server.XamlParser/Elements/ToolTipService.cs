e38925b3d454a3a2bfa338a0157bcb231f3edf12
BSick7 <brad.sickles@gmail.com>
2012-10-01 11:11:35 -0400

    Fixed release mode.

--- Source/WickedSick.Server.XamlParser/Elements/ToolTipService.cs
+++ Source/WickedSick.Server.XamlParser/Elements/ToolTipService.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using WickedSick.Server.XamlParser.Elements.Controls;
+﻿using WickedSick.Server.XamlParser.Elements.Controls;
 
 namespace WickedSick.Server.XamlParser.Elements
 {
-    public class ToolTipService: DependencyObject
+    public class ToolTipService : DependencyObject
     {
+        static ToolTipService() { }
         public static readonly AttachedPropertyDescription PlacementTarget = AttachedPropertyDescription.Register("PlacementTarget", typeof(UIElement), typeof(ToolTipService));
         public static readonly AttachedPropertyDescription ToolTip = AttachedPropertyDescription.Register("ToolTip", typeof(ToolTip), typeof(ToolTipService));
     }
