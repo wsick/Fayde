27320d66c19dc3f87813d26505a710fca2103002
BSick7 <brad.sickles@gmail.com>
2012-10-01 16:19:25 -0400

    Updated website.

--- Website/Fayde.Website/gettingstarted.aspx.cs
+++ Website/Fayde.Website/gettingstarted.aspx.cs
@@ -1,40 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Linq;
 
 namespace Fayde.Website
 {
     public partial class gettingstarted : System.Web.UI.Page
     {
-        private readonly string DEFAULT_FAP_XAML = 
-@"<FaydeApplication
-    xmlns=""WickedSick.Server.XamlParser;WickedSick.Server.XamlParser.Elements""
-    xmlns:x=""WickedSick.Server.XamlParser;WickedSick.Server.XamlParser.Core""
-    ScriptResolution=""scripts/"">
-    <FaydeApplication.UriMappings>
-        <UriMapping Uri="""" MappedUri=""/Views/home.fayde"" />
-        <UriMapping Uri=""/{pageName}"" MappedUri=""/Views/{pageName}.fayde"" />
-    </FaydeApplication.UriMappings>
-    <FaydeApplication.Resources>
-    </FaydeApplication.Resources>
-    <Grid x:Name=""LayoutRoot"">
-        <Frame x:Name=""ContentFrame"" />
-    </Grid>
-</FaydeApplication>";
+        private readonly List<string> DEFAULT_FAP_XAMLS = new List<string>{
+            "<FaydeApplication",
+            "\txmlns=\"WickedSick.Server.XamlParser;WickedSick.Server.XamlParser.Elements\"",
+            "\txmlns:x=\"WickedSick.Server.XamlParser;WickedSick.Server.XamlParser.Core\"",
+            "\tScriptResolution=\"scripts/\">",
+            "\t<FaydeApplication.UriMappings>",
+            "\t\t<UriMapping Uri=\"\" MappedUri=\"/Views/home.fayde\" />",
+            "\t</FaydeApplication.UriMappings>",
+            "\t<FaydeApplication.Resources>",
+            "\t</FaydeApplication.Resources>",
+            "\t<Grid x:Name=\"LayoutRoot\">",
+            "\t\t<Frame x:Name=\"ContentFrame\" />",
+            "\t</Grid>",
+            "</FaydeApplication>",
+        };
 
-        private readonly string HOME_XAML = 
-@"<Page xmlns=""WickedSick.Server.XamlParser;WickedSick.Server.XamlParser.Elements""
-      xmlns:x=""WickedSick.Server.XamlParser;WickedSick.Server.XamlParser.Core""
-      Title=""Home"">
-    <StackPanel>
-        <TextBlock Text=""Hello World!"" />
-    </StackPanel>
-</Page>";
+        private readonly List<string> HOME_XAMLS = new List<string>{
+            "<Page xmlns=\"WickedSick.Server.XamlParser;WickedSick.Server.XamlParser.Elements\"",
+            "\txmlns:x=\"WickedSick.Server.XamlParser;WickedSick.Server.XamlParser.Core\"",
+            "\tTitle=\"Home\">",
+            "\t<StackPanel>",
+            "\t\t<TextBlock Text=\"Hello World!\" />",
+            "\t</StackPanel>",
+            "</Page>",
+        };
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            ltrlDefaultFapXaml.Text = Server.HtmlEncode(DEFAULT_FAP_XAML);
-            ltrlHomeXaml.Text = Server.HtmlEncode(HOME_XAML);
+            ltrlDefaultFapXaml.Text = string.Join("<br />", DEFAULT_FAP_XAMLS.Select(x => Server.HtmlEncode(x).Replace("\t", "&nbsp;&nbsp;&nbsp;")));
+            ltrlHomeXaml.Text = string.Join("<br />", HOME_XAMLS.Select(x => Server.HtmlEncode(x).Replace("\t", "&nbsp;&nbsp;&nbsp;")));
         }
     }
 }
