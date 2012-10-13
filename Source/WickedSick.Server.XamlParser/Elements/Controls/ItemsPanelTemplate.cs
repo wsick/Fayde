ba0349c8af84e54611e6e9e0b9ed3975d5314891
BSick7 <brad.sickles@gmail.com>
2012-10-13 09:38:36 -0400

    Added ItemsControl.ItemsPanel property.

---
+++ Source/WickedSick.Server.XamlParser/Elements/Controls/ItemsPanelTemplate.cs
@@ -0,0 +1,8 @@
+﻿
+namespace WickedSick.Server.XamlParser.Elements.Controls
+{
+    public class ItemsPanelTemplate : DependencyObject
+    {
+        public static readonly PropertyDescription PanelProperty = PropertyDescription.Register("Panel", typeof(Panel), typeof(ItemsPanelTemplate), true);
+    }
+}
