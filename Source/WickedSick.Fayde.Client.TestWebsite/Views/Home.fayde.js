6057a4a65640864290b214a40013c7097cbc4a5d
BSick7 <brad.sickles@gmail.com>
2012-10-15 17:59:22 -0400

    Changed home to test bindings and ItemsSource.

--- Source/WickedSick.Fayde.Client.TestWebsite/Views/Home.fayde.js
+++ Source/WickedSick.Fayde.Client.TestWebsite/Views/Home.fayde.js
@@ -1,13 +1,19 @@
 ﻿(function () {
     var Home = Nullstone.Create("Home", Page);
 
+    var HEYO;
+
     Home.Instance.Init = function () {
         this.Init$Page();
         this.Loaded.Subscribe(this.OnLoaded, this);
     };
 
     Home.Instance.OnLoaded = function (o, e) {
-        alert("GET YOU SOME OF THAT. I LOADED BITCH!");
+        HEYO = this.FindName("HEYO");
+        setTimeout(function () {
+            debugger;
+            HEYO.ItemsSource = [{ Text: "I" }, { Text: "Heart" }, { Text: "Newbs" }];
+        }, 4000);
     };
 
     Nullstone.FinishCreate(Home);
