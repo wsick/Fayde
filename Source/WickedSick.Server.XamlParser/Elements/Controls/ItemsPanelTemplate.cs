812c1f7e53e8cf84f8a17cb4daeffa5b183c6907
BSick7 <brad.sickles@gmail.com>
2012-10-17 20:01:52 -0400

    Fixed ItemsPanelTemplate serialization.

--- Source/WickedSick.Server.XamlParser/Elements/Controls/ItemsPanelTemplate.cs
+++ Source/WickedSick.Server.XamlParser/Elements/Controls/ItemsPanelTemplate.cs
@@ -4,5 +4,17 @@ namespace WickedSick.Server.XamlParser.Elements.Controls
     public class ItemsPanelTemplate : DependencyObject
     {
         public static readonly PropertyDescription PanelProperty = PropertyDescription.Register("Panel", typeof(Panel), typeof(ItemsPanelTemplate), true);
+        public Panel Panel
+        {
+            get { return GetValue("Panel") as Panel; }
+            set { SetValue("Panel", value); }
+        }
+
+        public override string ToJson(int tabIndent)
+        {
+            if (Panel == null)
+                return "new ItemsPanelTemplate()";
+            return string.Format("new ItemsPanelTemplate({0})", Panel.ToJson(0));
+        }
     }
 }
