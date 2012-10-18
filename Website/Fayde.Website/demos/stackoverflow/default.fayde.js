f30a87224c473c5bd845fcf1c9f3974559e23cf8
BSick7 <brad.sickles@gmail.com>
2012-10-17 20:03:36 -0400

    Created stackoverflow demo.

---
+++ Website/Fayde.Website/demos/stackoverflow/default.fayde.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    var Root = Nullstone.Create("Root", Page);
+
+    Root.Instance.Init = function () {
+        this.Init$Page();
+        this.Loaded.Subscribe(this.OnLoaded, this);
+    };
+
+    var QuestionsItemsControl;
+    Root.Instance.OnLoaded = function (o, e) {
+        QuestionsItemsControl = Nullstone.As(this.FindName("QuestionsItemsControl"), ItemsControl);
+
+        var root = this;
+        var request = new AjaxJsonRequest(
+            function (json) { root.HandleQuestionResponse(json); },
+            function (error) { });
+        request.Get("so.ashx", "pagesize=10&order=desc&sort=activity&tagged=silverlight&site=stackoverflow");
+    };
+
+    Root.Instance.HandleQuestionResponse = function (json) {
+        debugger;
+        QuestionsItemsControl.ItemsSource = json.items;
+    };
+
+    Nullstone.FinishCreate(Root);
+
+    return Root;
+})()
---
+++ Website/Fayde.Website/demos/stackoverflow/so.ashx.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Web;
+
+namespace Fayde.Website.demos.stackoverflow
+{
+    public class so : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "application/json";
+            
+            var url = string.Format("https://api.stackexchange.com/2.1/questions?{0}", context.Request.Url.Query);
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            using (var sr = new StreamReader(response.GetEncodedStream(), System.Text.Encoding.UTF8))
+            {
+                context.Response.Write(sr.ReadToEnd());
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+
+    internal static class HttpWebResponseEx
+    {
+        public static Stream GetEncodedStream(this HttpWebResponse response)
+        {
+            var stream = response.GetResponseStream();
+            switch (response.ContentEncoding.ToUpperInvariant())
+            {
+                case "GZIP":
+                    return new GZipStream(stream, CompressionMode.Decompress);
+                case "DEFLATE":
+                    return new DeflateStream(stream, CompressionMode.Decompress);
+            }
+            return stream;
+        }
+    }
+}
