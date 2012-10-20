92639e65d1c8fa18cd8f57cb879af757b510f187
BSick7 <brad.sickles@gmail.com>
2012-10-20 11:26:46 -0400

    Added page caching and previous/next pages.  Still needs busy indicator.

--- Website/Fayde.Website/demos/stackoverflow/default.fayde.js
+++ Website/Fayde.Website/demos/stackoverflow/default.fayde.js
@@ -7,19 +7,42 @@
     };
 
     var QuestionsItemsControl;
+    var PreviousPageButton;
+    var NextPageButton;
+    var PageNumber = 1;
+    var Request;
     Root.Instance.OnLoaded = function (o, e) {
         QuestionsItemsControl = Nullstone.As(this.FindName("QuestionsItemsControl"), ItemsControl);
+        PreviousPageButton = Nullstone.As(this.FindName("PreviousPageButton"), Button);
+        NextPageButton = Nullstone.As(this.FindName("NextPageButton"), Button);
 
-        var root = this;
-        var request = new AjaxJsonRequest(
-            function (json) { root.HandleQuestionResponse(json); },
-            function (error) { });
-        request.Get("so.ashx", "pagesize=10&order=desc&sort=activity&tagged=silverlight&site=stackoverflow");
+        NextPageButton.Click.Subscribe(this.Next_Click, this);
+        this.GetPage(1);
     };
 
     Root.Instance.HandleQuestionResponse = function (json) {
-        debugger;
+        //debugger;
         QuestionsItemsControl.ItemsSource = json.items;
+    };
+
+    Root.Instance.Previous_Click = function (o, e) {
+        PageNumber--;
+        this.GetPage(PageNumber);
+    };
+
+    Root.Instance.Next_Click = function (o, e) {
+        PageNumber++;
+        this.GetPage(PageNumber);
+    };
+
+    Root.Instance.GetPage = function (page) {
+        if (Request != null)
+            return;
+        var root = this;
+        Request = new AjaxJsonRequest(
+            function (json) { Request = null; root.HandleQuestionResponse(json); },
+            function (error) { Request = null; });
+        Request.Get("so.ashx", "tagged=silverlight&sort=activity&page=" + page);
     };
 
     Nullstone.FinishCreate(Root);
--- Website/Fayde.Website/demos/stackoverflow/so.ashx.cs
+++ Website/Fayde.Website/demos/stackoverflow/so.ashx.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.IO.Compression;
 using System.Net;
 using System.Web;
@@ -10,16 +12,17 @@ namespace Fayde.Website.demos.stackoverflow
         public void ProcessRequest(HttpContext context)
         {
             context.Response.ContentType = "application/json";
-            
-            var url = string.Format("https://api.stackexchange.com/2.1/questions?{0}", context.Request.Url.Query);
 
-            var request = (HttpWebRequest)WebRequest.Create(url);
+            var cache = context.Application["Cache"] as Cache;
+            if (cache == null)
+                context.Application["Cache"] = cache = new Cache();
 
-            using (var response = (HttpWebResponse)request.GetResponse())
-            using (var sr = new StreamReader(response.GetEncodedStream(), System.Text.Encoding.UTF8))
-            {
-                context.Response.Write(sr.ReadToEnd());
-            }
+            string query = string.Format("tagged={0}&sort={1}", context.Request.QueryString["tagged"], context.Request.QueryString["sort"]);
+            int pageNumber = 0;
+            int.TryParse(context.Request.QueryString["page"], out pageNumber);
+            var json = cache.GetPageJson(pageNumber, query);
+
+            context.Response.Write(json);
         }
 
         public bool IsReusable
@@ -29,6 +32,55 @@ namespace Fayde.Website.demos.stackoverflow
                 return false;
             }
         }
+    }
+
+    internal class Cache
+    {
+        public Cache()
+        {
+            Pages = new Dictionary<int, PageCache>();
+        }
+
+        public Dictionary<int, PageCache> Pages { get; set; }
+
+        public string GetPageJson(int pageNumber, string query)
+        {
+            if (!Pages.ContainsKey(pageNumber))
+                Pages[pageNumber] = RefreshPageCache(pageNumber, query);
+            else if ((DateTime.UtcNow - Pages[pageNumber].RequestTime).TotalHours > 12)
+                Pages[pageNumber] = RefreshPageCache(pageNumber, query);
+
+            return Pages[pageNumber].JsonResponse;
+        }
+
+        private PageCache RefreshPageCache(int pageNumber, string query)
+        {
+            string json = null;
+            if (pageNumber > 1)
+                json = GetQuestionsFromStackOverflow(string.Format("{0}&pagesize=10&page={1}", query, pageNumber));
+            else
+                json = GetQuestionsFromStackOverflow(string.Format("{0}&pagesize=10", query));
+            return new PageCache { JsonResponse = json, RequestTime = DateTime.UtcNow, };
+        }
+
+        private string GetQuestionsFromStackOverflow(string query)
+        {
+            var url = string.Format("https://api.stackexchange.com/2.1/questions?{0}&pagesize=10&order=desc&site=stackoverflow", query);
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            using (var sr = new StreamReader(response.GetEncodedStream(), System.Text.Encoding.UTF8))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+
+    internal class PageCache
+    {
+        public string JsonResponse { get; set; }
+        public DateTime RequestTime { get; set; }
     }
 
     internal static class HttpWebResponseEx
