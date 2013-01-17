a849be578f200de31921690db7cbc35d0793fa45
BSick7 <brad.sickles@gmail.com>
2013-01-17 13:42:47 -0500

    Migrated engine to use module loader.

--- Source/WickedSick.Fayde.Client.Engine/Javascript/Engine/Dirty.js
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Engine/Dirty.js
@@ -3,82 +3,83 @@
 /// CODE
 /// <reference path="../Core/Enums.js"/>
 
-var _Dirty = {
-    Transform: 1 << 0,
-    LocalTransform: 1 << 1,
-    LocalProjection: 1 << 2,
-    Clip: 1 << 3,
-    LocalClip: 1 << 4,
-    RenderVisibility: 1 << 5,
-    HitTestVisibility: 1 << 6,
-    Measure: 1 << 7,
-    Arrange: 1 << 8,
-    ChildrenZIndices: 1 << 9,
-    Bounds: 1 << 20,
-    NewBounds: 1 << 21,
-    Invalidate: 1 << 22,
-    InUpDirtyList: 1 << 30,
-    InDownDirtyList: 1 << 31
-};
-_Dirty.DownDirtyState =
-    _Dirty.Transform |
-    _Dirty.LocalTransform |
-    _Dirty.LocalProjection |
-    _Dirty.Clip |
-    _Dirty.LocalClip |
-    _Dirty.RenderVisibility |
-    _Dirty.HitTestVisibility |
-    _Dirty.ChildrenZIndices;
-_Dirty.UpDirtyState = _Dirty.Bounds | _Dirty.Invalidate;
-_Dirty.State = _Dirty.DownDirtyState | _Dirty.UpDirtyState;
+(function (namespace) {
+    namespace._Dirty = {
+        Transform: 1 << 0,
+        LocalTransform: 1 << 1,
+        LocalProjection: 1 << 2,
+        Clip: 1 << 3,
+        LocalClip: 1 << 4,
+        RenderVisibility: 1 << 5,
+        HitTestVisibility: 1 << 6,
+        Measure: 1 << 7,
+        Arrange: 1 << 8,
+        ChildrenZIndices: 1 << 9,
+        Bounds: 1 << 20,
+        NewBounds: 1 << 21,
+        Invalidate: 1 << 22,
+        InUpDirtyList: 1 << 30,
+        InDownDirtyList: 1 << 31
+    };
+    _Dirty.DownDirtyState =
+        _Dirty.Transform |
+        _Dirty.LocalTransform |
+        _Dirty.LocalProjection |
+        _Dirty.Clip |
+        _Dirty.LocalClip |
+        _Dirty.RenderVisibility |
+        _Dirty.HitTestVisibility |
+        _Dirty.ChildrenZIndices;
+    _Dirty.UpDirtyState = _Dirty.Bounds | _Dirty.Invalidate;
+    _Dirty.State = _Dirty.DownDirtyState | _Dirty.UpDirtyState;
 
-_Dirty.__DebugToString = function (dirty) {
-    switch (dirty) {
-        case _Dirty.Measure:
-            return "[Measure]";
-        case _Dirty.Arrange:
-            return "[Arrange]";
-        case _Dirty.Bounds:
-            return "[Bounds]";
-        case _Dirty.NewBounds:
-            return "[NewBounds]";
-        case _Dirty.ChildrenZIndices:
-            return "[ChildrenZIndices]";
-        case _Dirty.Clip:
-            return "[Clip]";
-        case _Dirty.Invalidate:
-            return "[Invalidate]";
-        case _Dirty.Transform:
-            return "[Transform]";
-        case _Dirty.LocalTransform:
-            return "[LocalTransform]";
-        case _Dirty.LocalProjection:
-            return "[LocalProjection]";
-        case _Dirty.RenderVisibility:
-            return "[RenderVisibility]";
-        case _Dirty.HitTestVisibility:
-            return "[HitTestVisibility]";
-    }
-    return dirty;
-};
+    _Dirty.__DebugToString = function (dirty) {
+        switch (dirty) {
+            case _Dirty.Measure:
+                return "[Measure]";
+            case _Dirty.Arrange:
+                return "[Arrange]";
+            case _Dirty.Bounds:
+                return "[Bounds]";
+            case _Dirty.NewBounds:
+                return "[NewBounds]";
+            case _Dirty.ChildrenZIndices:
+                return "[ChildrenZIndices]";
+            case _Dirty.Clip:
+                return "[Clip]";
+            case _Dirty.Invalidate:
+                return "[Invalidate]";
+            case _Dirty.Transform:
+                return "[Transform]";
+            case _Dirty.LocalTransform:
+                return "[LocalTransform]";
+            case _Dirty.LocalProjection:
+                return "[LocalProjection]";
+            case _Dirty.RenderVisibility:
+                return "[RenderVisibility]";
+            case _Dirty.HitTestVisibility:
+                return "[HitTestVisibility]";
+        }
+        return dirty;
+    };
 
-//#region _DirtyList
-var _DirtyList = Nullstone.Create("_DirtyList", LinkedList, 1);
 
-_DirtyList.Instance.Init = function (type) {
-    this.Init$LinkedList();
-    this._Type = type;
-};
+    var _DirtyList = Nullstone.Create("_DirtyList", LinkedList, 1);
 
-_DirtyList.Instance.__DebugToString = function () {
-    var s = "";
-    var node = this.Head;
-    while (node) {
-        s += "[" + node.UIElement.__DebugToString() + "]";
-        node = node.Next;
-    }
-    return s;
-};
+    _DirtyList.Instance.Init = function (type) {
+        this.Init$LinkedList();
+        this._Type = type;
+    };
 
-Nullstone.FinishCreate(_DirtyList);
-//#endregion
+    _DirtyList.Instance.__DebugToString = function () {
+        var s = "";
+        var node = this.Head;
+        while (node) {
+            s += "[" + node.UIElement.__DebugToString() + "]";
+            node = node.Next;
+        }
+        return s;
+    };
+
+    namespace._DirtyList = Nullstone.FinishCreate(_DirtyList);
+})(window);
--- Source/WickedSick.Fayde.Client.Engine/Javascript/Engine/RenderContext.js
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Engine/RenderContext.js
@@ -5,174 +5,174 @@
 /// <reference path="../../gl-matrix.js"/>
 /// <reference path="../Runtime/gl-matrix-ex.js"/>
 
-//#region _RenderContext
-var _RenderContext = Nullstone.Create("_RenderContext", undefined, 1);
+(function (namespace) {
+    var _RenderContext = Nullstone.Create("_RenderContext", undefined, 1);
 
-_RenderContext.Instance.Init = function (surface) {
-    this.Surface = surface;
-    this.CanvasContext = this.Surface._Ctx;
-    this._Transforms = [];
+    _RenderContext.Instance.Init = function (surface) {
+        this.Surface = surface;
+        this.CanvasContext = this.Surface._Ctx;
+        this._Transforms = [];
 
-    if (!this.CanvasContext.hasOwnProperty("currentTransform")) {
-        Object.defineProperty(this.CanvasContext, "currentTransform", {
-            get: function () { return this._CurrentTransform; },
-            set: function (value) {
-                this.setTransform(value[0], value[1], value[3], value[4], value[2], value[5]);
-                this._CurrentTransform = value;
-            }
-        });
-    }
-};
+        if (!this.CanvasContext.hasOwnProperty("currentTransform")) {
+            Object.defineProperty(this.CanvasContext, "currentTransform", {
+                get: function () { return this._CurrentTransform; },
+                set: function (value) {
+                    this.setTransform(value[0], value[1], value[3], value[4], value[2], value[5]);
+                    this._CurrentTransform = value;
+                }
+            });
+        }
+    };
 
-Nullstone.AutoProperties(_RenderContext, [
-    "CurrentTransform",
-    "CanvasContext"
-]);
+    Nullstone.AutoProperties(_RenderContext, [
+        "CurrentTransform",
+        "CanvasContext"
+    ]);
 
-//#region Clip/Point Test
+    //#region Clip/Point Test
 
-_RenderContext.Instance.Clip = function (clip) {
-    this._DrawClip(clip);
-    this.CanvasContext.clip();
-};
-_RenderContext.Instance.IsPointInPath = function (p) {
-    return this.CanvasContext.isPointInPath(p.X, p.Y);
-};
-_RenderContext.Instance.IsPointInClipPath = function (clip, p) {
-    this._DrawClip(clip);
-    return this.CanvasContext.isPointInPath(p.X, p.Y);
-};
-_RenderContext.Instance._DrawClip = function (clip) {
-    if (clip instanceof Rect) {
-        this.CanvasContext.beginPath();
-        this.CanvasContext.rect(clip.X, clip.Y, clip.Width, clip.Height);
-        DrawDebug("DrawClip (Rect): " + clip.toString());
-    } else if (clip instanceof Geometry) {
-        clip.Draw(this);
-        DrawDebug("DrawClip (Geometry): " + clip.toString());
-    } else if (clip instanceof RawPath) {
-        clip.Draw(this);
-        DrawDebug("DrawClip (RawPath): " + clip.toString());
-    }
-};
+    _RenderContext.Instance.Clip = function (clip) {
+        this._DrawClip(clip);
+        this.CanvasContext.clip();
+    };
+    _RenderContext.Instance.IsPointInPath = function (p) {
+        return this.CanvasContext.isPointInPath(p.X, p.Y);
+    };
+    _RenderContext.Instance.IsPointInClipPath = function (clip, p) {
+        this._DrawClip(clip);
+        return this.CanvasContext.isPointInPath(p.X, p.Y);
+    };
+    _RenderContext.Instance._DrawClip = function (clip) {
+        if (clip instanceof Rect) {
+            this.CanvasContext.beginPath();
+            this.CanvasContext.rect(clip.X, clip.Y, clip.Width, clip.Height);
+            DrawDebug("DrawClip (Rect): " + clip.toString());
+        } else if (clip instanceof Geometry) {
+            clip.Draw(this);
+            DrawDebug("DrawClip (Geometry): " + clip.toString());
+        } else if (clip instanceof RawPath) {
+            clip.Draw(this);
+            DrawDebug("DrawClip (RawPath): " + clip.toString());
+        }
+    };
 
-//#endregion
+    //#endregion
 
-//#region Transforms
+    //#region Transforms
 
-_RenderContext.Instance.PreTransform = function (matrix) {
-    if (matrix instanceof Transform) {
-        matrix = matrix.Value.raw;
-    }
+    _RenderContext.Instance.PreTransform = function (matrix) {
+        if (matrix instanceof Transform) {
+            matrix = matrix.Value.raw;
+        }
 
-    var ct = this.CurrentTransform;
-    mat3.multiply(matrix, ct, ct); //ct = ct * matrix
-    this.CanvasContext.currentTransform = ct;
+        var ct = this.CurrentTransform;
+        mat3.multiply(matrix, ct, ct); //ct = ct * matrix
+        this.CanvasContext.currentTransform = ct;
 
-    TransformDebug("PreTransform", ct);
-};
-_RenderContext.Instance.Transform = function (matrix) {
-    if (matrix instanceof Transform) {
-        matrix = matrix.Value.raw;
-    }
+        TransformDebug("PreTransform", ct);
+    };
+    _RenderContext.Instance.Transform = function (matrix) {
+        if (matrix instanceof Transform) {
+            matrix = matrix.Value.raw;
+        }
 
-    var ct = this.CurrentTransform;
-    mat3.multiply(ct, matrix, ct); //ct = matrix * ct
-    this.CanvasContext.currentTransform = ct;
+        var ct = this.CurrentTransform;
+        mat3.multiply(ct, matrix, ct); //ct = matrix * ct
+        this.CanvasContext.currentTransform = ct;
 
-    TransformDebug("Transform", ct);
-};
-_RenderContext.Instance.Translate = function (x, y) {
-    var ct = this.CurrentTransform;
-    mat3.translate(ct, x, y);
-    this.CanvasContext.translate(x, y);
+        TransformDebug("Transform", ct);
+    };
+    _RenderContext.Instance.Translate = function (x, y) {
+        var ct = this.CurrentTransform;
+        mat3.translate(ct, x, y);
+        this.CanvasContext.translate(x, y);
 
-    TransformDebug("Translate", ct);
-};
+        TransformDebug("Translate", ct);
+    };
 
-//#endregion
+    //#endregion
 
-//#region State
+    //#region State
 
-_RenderContext.Instance.Save = function () {
-    this.CanvasContext.save();
-    var ct = this.CurrentTransform;
-    this._Transforms.push(ct);
-    this.CurrentTransform = ct == null ? mat3.identity() : mat3.create(ct);
-    if (this.CurrentTransform)
-        TransformDebug("Save", this.CurrentTransform);
-};
-_RenderContext.Instance.Restore = function () {
-    var curXform = this._Transforms.pop();
-    this.CurrentTransform = curXform;
-    this.CanvasContext.restore();
-    if (this.CurrentTransform)
-        TransformDebug("Restore", this.CurrentTransform);
-};
+    _RenderContext.Instance.Save = function () {
+        this.CanvasContext.save();
+        var ct = this.CurrentTransform;
+        this._Transforms.push(ct);
+        this.CurrentTransform = ct == null ? mat3.identity() : mat3.create(ct);
+        if (this.CurrentTransform)
+            TransformDebug("Save", this.CurrentTransform);
+    };
+    _RenderContext.Instance.Restore = function () {
+        var curXform = this._Transforms.pop();
+        this.CurrentTransform = curXform;
+        this.CanvasContext.restore();
+        if (this.CurrentTransform)
+            TransformDebug("Restore", this.CurrentTransform);
+    };
 
-//#endregion
+    //#endregion
 
-//#region Stroke/Fill/Clear
+    //#region Stroke/Fill/Clear
 
-_RenderContext.Instance.Rect = function (rect) {
-    var ctx = this.CanvasContext;
-    ctx.beginPath();
-    ctx.rect(rect.X, rect.Y, rect.Width, rect.Height);
-    DrawDebug("Rect: " + rect.toString());
-};
-_RenderContext.Instance.Fill = function (brush, region) {
-    /// <param name="brush" type="Brush"></param>
-    var ctx = this.CanvasContext;
-    brush.SetupBrush(ctx, region);
-    ctx.fillStyle = brush.ToHtml5Object();
-    ctx.fill();
-    DrawDebug("Fill: [" + ctx.fillStyle.toString() + "]");
-};
-_RenderContext.Instance.FillRect = function (brush, rect) {
-    /// <param name="brush" type="Brush"></param>
-    /// <param name="rect" type="Rect"></param>
-    var ctx = this.CanvasContext;
-    brush.SetupBrush(ctx, rect);
-    ctx.beginPath();
-    ctx.rect(rect.X, rect.Y, rect.Width, rect.Height);
-    ctx.fillStyle = brush.ToHtml5Object();
-    ctx.fill();
-    DrawDebug("FillRect: [" + ctx.fillStyle.toString() + "] " + rect.toString());
-};
-_RenderContext.Instance.StrokeAndFillRect = function (strokeBrush, thickness, strokeRect, fillBrush, fillRect) {
-    var ctx = this.CanvasContext;
-    strokeBrush.SetupBrush(ctx, strokeRect);
-    fillBrush.SetupBrush(ctx, fillRect);
-    ctx.beginPath();
-    ctx.rect(fillRect.X, fillRect.Y, fillRect.Width, fillRect.Height);
-    ctx.strokeStyle = strokeBrush.ToHtml5Object();
-    ctx.stroke();
-    ctx.fillStyle = fillBrush.ToHtml5Object();
-    ctx.fill();
-    DrawDebug("StrokeAndFillRect: [" + ctx.strokeStyle.toString() + "] [" + ctx.fillStyle.toString() + "] " + fillRect.toString());
-};
-_RenderContext.Instance.Stroke = function (stroke, thickness, region) {
-    /// <param name="stroke" type="Brush"></param>
-    var ctx = this.CanvasContext;
-    stroke.SetupBrush(ctx, region);
-    ctx.lineWidth = thickness;
-    ctx.strokeStyle = stroke.ToHtml5Object();
-    ctx.stroke();
-    DrawDebug("Stroke: [" + ctx.strokeStyle.toString() + "] -> " + ctx.lineWidth.toString());
-};
-_RenderContext.Instance.Clear = function (rect) {
-    this.CanvasContext.clearRect(rect.X, rect.Y, rect.Width, rect.Height);
-    DrawDebug("Clear: " + rect.toString());
-};
+    _RenderContext.Instance.Rect = function (rect) {
+        var ctx = this.CanvasContext;
+        ctx.beginPath();
+        ctx.rect(rect.X, rect.Y, rect.Width, rect.Height);
+        DrawDebug("Rect: " + rect.toString());
+    };
+    _RenderContext.Instance.Fill = function (brush, region) {
+        /// <param name="brush" type="Brush"></param>
+        var ctx = this.CanvasContext;
+        brush.SetupBrush(ctx, region);
+        ctx.fillStyle = brush.ToHtml5Object();
+        ctx.fill();
+        DrawDebug("Fill: [" + ctx.fillStyle.toString() + "]");
+    };
+    _RenderContext.Instance.FillRect = function (brush, rect) {
+        /// <param name="brush" type="Brush"></param>
+        /// <param name="rect" type="Rect"></param>
+        var ctx = this.CanvasContext;
+        brush.SetupBrush(ctx, rect);
+        ctx.beginPath();
+        ctx.rect(rect.X, rect.Y, rect.Width, rect.Height);
+        ctx.fillStyle = brush.ToHtml5Object();
+        ctx.fill();
+        DrawDebug("FillRect: [" + ctx.fillStyle.toString() + "] " + rect.toString());
+    };
+    _RenderContext.Instance.StrokeAndFillRect = function (strokeBrush, thickness, strokeRect, fillBrush, fillRect) {
+        var ctx = this.CanvasContext;
+        strokeBrush.SetupBrush(ctx, strokeRect);
+        fillBrush.SetupBrush(ctx, fillRect);
+        ctx.beginPath();
+        ctx.rect(fillRect.X, fillRect.Y, fillRect.Width, fillRect.Height);
+        ctx.strokeStyle = strokeBrush.ToHtml5Object();
+        ctx.stroke();
+        ctx.fillStyle = fillBrush.ToHtml5Object();
+        ctx.fill();
+        DrawDebug("StrokeAndFillRect: [" + ctx.strokeStyle.toString() + "] [" + ctx.fillStyle.toString() + "] " + fillRect.toString());
+    };
+    _RenderContext.Instance.Stroke = function (stroke, thickness, region) {
+        /// <param name="stroke" type="Brush"></param>
+        var ctx = this.CanvasContext;
+        stroke.SetupBrush(ctx, region);
+        ctx.lineWidth = thickness;
+        ctx.strokeStyle = stroke.ToHtml5Object();
+        ctx.stroke();
+        DrawDebug("Stroke: [" + ctx.strokeStyle.toString() + "] -> " + ctx.lineWidth.toString());
+    };
+    _RenderContext.Instance.Clear = function (rect) {
+        this.CanvasContext.clearRect(rect.X, rect.Y, rect.Width, rect.Height);
+        DrawDebug("Clear: " + rect.toString());
+    };
 
-//#endregion
+    //#endregion
 
-_RenderContext.ToArray = function (args) {
-    var arr = [];
-    for (var i in args)
-        arr.push(args[i]);
-    return arr;
-};
+    _RenderContext.ToArray = function (args) {
+        var arr = [];
+        for (var i in args)
+            arr.push(args[i]);
+        return arr;
+    };
 
-Nullstone.FinishCreate(_RenderContext);
-//#endregion
+    namespace._RenderContext = Nullstone.FinishCreate(_RenderContext);
+})(window);
