3d52c22be59801b9d1f84e00739b8d90ad05827f
BSick7 <brad.sickles@gmail.com>
2013-01-17 11:54:01 -0500

    Migrated shapes to use module loader.

--- Source/WickedSick.Fayde.Client.Engine/Javascript/Shapes/Path.js
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Shapes/Path.js
@@ -3,73 +3,73 @@
 /// <reference path="../Media/Geometry.js"/>
 /// <reference path="../Core/Core.js"/>
 
-//#region Path
-var Path = Nullstone.Create("Path", Shape);
+(function (namespace) {
+    var Path = Nullstone.Create("Path", Shape);
 
-Path.Instance.Init = function () {
-    this.Init$Shape();
-};
+    Path.Instance.Init = function () {
+        this.Init$Shape();
+    };
 
-//#region Dependency Properties
+    //#region Properties
 
-// Path.Data Description: http://msdn.microsoft.com/en-us/library/system.windows.shapes.path.data(v=vs.95).aspx
-Path.DataProperty = DependencyProperty.RegisterCore("Data", function () { return Geometry; }, Path);
+    // Path.Data Description: http://msdn.microsoft.com/en-us/library/system.windows.shapes.path.data(v=vs.95).aspx
+    Path.DataProperty = DependencyProperty.RegisterCore("Data", function () { return Geometry; }, Path);
 
-Nullstone.AutoProperty(Path, Path.DataProperty, function (value) {
-    if (value instanceof Geometry)
+    Nullstone.AutoProperty(Path, Path.DataProperty, function (value) {
+        if (value instanceof Geometry)
+            return value;
+        if (typeof value === "string")
+            return Fayde.TypeConverter.GeometryFromString(value);
         return value;
-    if (typeof value === "string")
-        return Fayde.TypeConverter.GeometryFromString(value);
-    return value;
-});
+    });
 
-//#endregion
+    //#endregion
 
-Path.Instance._GetFillRule = function () {
-    var geom = this.Data;
-    if (geom == null)
-        return this._GetFillRule$Shape();
-    return geom.FillRule;
-};
+    Path.Instance._GetFillRule = function () {
+        var geom = this.Data;
+        if (geom == null)
+            return this._GetFillRule$Shape();
+        return geom.FillRule;
+    };
 
-Path.Instance._DrawPath = function (ctx) {
-    /// <param name="ctx" type="_RenderContext"></param>
-    var geom = this.Data;
-    if (geom == null)
-        return;
-    geom.Draw(ctx);
-};
+    Path.Instance._DrawPath = function (ctx) {
+        /// <param name="ctx" type="_RenderContext"></param>
+        var geom = this.Data;
+        if (geom == null)
+            return;
+        geom.Draw(ctx);
+    };
 
-Path.Instance._ComputeShapeBoundsImpl = function (logical, matrix) {
-    /// <param name="logical" type="Boolean"></param>
-    /// <param name="matrix" type="Matrix"></param>
-    /// <returns type="Rect" />
-    var geom = this.Data;
-    if (geom == null) {
-        this._SetShapeFlags(ShapeFlags.Empty);
-        return new Rect();
-    }
-    if (logical)
-        return geom.GetBounds();
+    Path.Instance._ComputeShapeBoundsImpl = function (logical, matrix) {
+        /// <param name="logical" type="Boolean"></param>
+        /// <param name="matrix" type="Matrix"></param>
+        /// <returns type="Rect" />
+        var geom = this.Data;
+        if (geom == null) {
+            this._SetShapeFlags(ShapeFlags.Empty);
+            return new Rect();
+        }
+        if (logical)
+            return geom.GetBounds();
 
-    var thickness = (logical || !this._IsStroked()) ? 0.0 : this.StrokeThickness;
-    return geom.GetBounds(thickness);
-    return shapeBounds;
-};
+        var thickness = (logical || !this._IsStroked()) ? 0.0 : this.StrokeThickness;
+        return geom.GetBounds(thickness);
+        return shapeBounds;
+    };
 
-Path.Instance._OnPropertyChanged = function (args, error) {
-    if (args.Property.OwnerType !== Path) {
-        this._OnPropertyChanged$Shape(args, error);
-        return;
-    }
-};
-Path.Instance._OnSubPropertyChanged = function (propd, sender, args) {
-    if (propd != null && propd._ID === Path.DataProperty._ID) {
-        this._InvalidateNaturalBounds();
-        return;
-    }
-    this._OnSubPropertyChanged$Shape(propd, sender, args);
-};
+    Path.Instance._OnPropertyChanged = function (args, error) {
+        if (args.Property.OwnerType !== Path) {
+            this._OnPropertyChanged$Shape(args, error);
+            return;
+        }
+    };
+    Path.Instance._OnSubPropertyChanged = function (propd, sender, args) {
+        if (propd != null && propd._ID === Path.DataProperty._ID) {
+            this._InvalidateNaturalBounds();
+            return;
+        }
+        this._OnSubPropertyChanged$Shape(propd, sender, args);
+    };
 
-Nullstone.FinishCreate(Path);
-//#endregion
+    namespace.Path = Nullstone.FinishCreate(Path);
+})(window);
--- Source/WickedSick.Fayde.Client.Engine/Javascript/Shapes/Polyline.js
+++ Source/WickedSick.Fayde.Client.Engine/Javascript/Shapes/Polyline.js
@@ -3,77 +3,77 @@
 /// <reference path="Enums.js"/>
 /// <reference path="PointCollection.js"/>
 
-//#region Polyline
-var Polyline = Nullstone.Create("Polyline", Shape);
+(function (namespace) {
+    var Polyline = Nullstone.Create("Polyline", Shape);
 
-Polyline.Instance.Init = function () {
-    this.Init$Shape();
-};
+    Polyline.Instance.Init = function () {
+        this.Init$Shape();
+    };
 
-//#region Properties
+    //#region Properties
 
-Polyline.FillRuleProperty = DependencyProperty.RegisterCore("FillRule", function () { return new Enum(FillRule); }, Polyline, FillRule.EvenOdd);
-Polyline.PointsProperty = DependencyProperty.RegisterFull("Points", function () { return PointCollection; }, Polyline, undefined, undefined, { GetValue: function () { return new PointCollection(); } });
+    Polyline.FillRuleProperty = DependencyProperty.RegisterCore("FillRule", function () { return new Enum(FillRule); }, Polyline, FillRule.EvenOdd);
+    Polyline.PointsProperty = DependencyProperty.RegisterFull("Points", function () { return PointCollection; }, Polyline, undefined, undefined, { GetValue: function () { return new PointCollection(); } });
 
-Nullstone.AutoProperties(Polyline, [
-    Polyline.FillRuleProperty
-]);
-Nullstone.AutoProperty(Polyline, Polyline.PointsProperty, function (value) {
-    if (value instanceof PointCollection)
+    Nullstone.AutoProperties(Polyline, [
+        Polyline.FillRuleProperty
+    ]);
+    Nullstone.AutoProperty(Polyline, Polyline.PointsProperty, function (value) {
+        if (value instanceof PointCollection)
+            return value;
+        if (typeof value === "string")
+            return Fayde.TypeConverter.PointCollectionFromString(value);
         return value;
-    if (typeof value === "string")
-        return Fayde.TypeConverter.PointCollectionFromString(value);
-    return value;
-});
+    });
 
-//#endregion
+    //#endregion
 
-Polyline.Instance._BuildPath = function () {
-    var points = this.Points;
-    var count;
-    if (points == null || (count = points.GetCount()) < 2) {
-        this._SetShapeFlags(ShapeFlags.Empty);
-        return;
-    }
+    Polyline.Instance._BuildPath = function () {
+        var points = this.Points;
+        var count;
+        if (points == null || (count = points.GetCount()) < 2) {
+            this._SetShapeFlags(ShapeFlags.Empty);
+            return;
+        }
 
-    this._SetShapeFlags(ShapeFlags.Normal);
+        this._SetShapeFlags(ShapeFlags.Normal);
 
-    this._Path = new RawPath();
-    var p = points.GetValueAt(0);
-    this._Path.Move(p.X, p.Y);
+        this._Path = new RawPath();
+        var p = points.GetValueAt(0);
+        this._Path.Move(p.X, p.Y);
 
-    for (var i = 1; i < count; i++) {
-        p = points.GetValueAt(i);
-        this._Path.Line(p.X, p.Y);
-    }
-};
+        for (var i = 1; i < count; i++) {
+            p = points.GetValueAt(i);
+            this._Path.Line(p.X, p.Y);
+        }
+    };
 
-Polyline.Instance._CanFill = function () { return true; };
+    Polyline.Instance._CanFill = function () { return true; };
 
-Polyline.Instance._OnPropertyChanged = function (args, error) {
-    if (args.Property.OwnerType !== Polyline) {
-        this._OnPropertyChanged$Shape(args, error);
-        return;
-    }
+    Polyline.Instance._OnPropertyChanged = function (args, error) {
+        if (args.Property.OwnerType !== Polyline) {
+            this._OnPropertyChanged$Shape(args, error);
+            return;
+        }
 
-    if (args.Property._ID === Polyline.PointsProperty._ID) {
+        if (args.Property._ID === Polyline.PointsProperty._ID) {
+            this._InvalidateNaturalBounds();
+        }
+
+        this._Invalidate();
+        this.PropertyChanged.Raise(this, args);
+    };
+    Polyline.Instance._OnCollectionChanged = function (col, args) {
+        if (!this._PropertyHasValueNoAutoCreate(Polyline.PointsProperty, col)) {
+            this._OnCollectionChanged$Shape(col, args);
+            return;
+        }
         this._InvalidateNaturalBounds();
-    }
+    };
+    Polyline.Instance._OnCollectionItemChanged = function (col, obj, args) {
+        this._OnCollectionItemChanged$Shape(col, obj, args);
+        this._InvalidateNaturalBounds();
+    };
 
-    this._Invalidate();
-    this.PropertyChanged.Raise(this, args);
-};
-Polyline.Instance._OnCollectionChanged = function (col, args) {
-    if (!this._PropertyHasValueNoAutoCreate(Polyline.PointsProperty, col)) {
-        this._OnCollectionChanged$Shape(col, args);
-        return;
-    }
-    this._InvalidateNaturalBounds();
-};
-Polyline.Instance._OnCollectionItemChanged = function (col, obj, args) {
-    this._OnCollectionItemChanged$Shape(col, obj, args);
-    this._InvalidateNaturalBounds();
-};
-
-Nullstone.FinishCreate(Polyline);
-//#endregion
+    namespace.Polyline = Nullstone.FinishCreate(Polyline);
+})(window);
