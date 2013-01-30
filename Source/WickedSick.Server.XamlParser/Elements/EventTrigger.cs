56b12bd278bb6f46e861500c178a08e76d060dff
BSick7 <brad.sickles@gmail.com>
2013-01-30 11:02:17 -0500

    Neglected to move server-side triggers to Fayde namespace.

--- Source/WickedSick.Server.XamlParser/Elements/EventTrigger.cs
+++ Source/WickedSick.Server.XamlParser/Elements/EventTrigger.cs
@@ -1,9 +1,10 @@
 ﻿
 namespace WickedSick.Server.XamlParser.Elements
 {
+    [Element(NullstoneNamespace = "Fayde")]
     public class EventTrigger : TriggerBase
     {
-        public static readonly PropertyDescription Actions = PropertyDescription.Register("Actions", typeof(DependencyObjectCollection<TriggerAction>), typeof(EventTrigger), true);
-        public static readonly PropertyDescription RoutedEvent = PropertyDescription.Register("RoutedEvent", typeof(string), typeof(EventTrigger));
+        public static readonly PropertyDescription ActionsProperty = PropertyDescription.Register("Actions", typeof(DependencyObjectCollection<TriggerAction>), typeof(EventTrigger), true);
+        public static readonly PropertyDescription RoutedEventProperty = PropertyDescription.Register("RoutedEvent", typeof(string), typeof(EventTrigger));
     }
 }
--- Source/WickedSick.Server.XamlParser/Elements/TriggerAction.cs
+++ Source/WickedSick.Server.XamlParser/Elements/TriggerAction.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace WickedSick.Server.XamlParser.Elements
 {
+    [Element(NullstoneNamespace = "Fayde")]
     public abstract class TriggerAction : DependencyObject
     {
     }
