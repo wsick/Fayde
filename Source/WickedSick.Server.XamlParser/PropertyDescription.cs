e5a901809658b4a92cb0cf7e679d0b4fbe0a3bf0
BSick7 <brad.sickles@gmail.com>
2013-02-15 16:38:56 -0500

    Parser optimization. Dependency Property registration is now indexed by owner type, then by name.

--- Source/WickedSick.Server.XamlParser/PropertyDescription.cs
+++ Source/WickedSick.Server.XamlParser/PropertyDescription.cs
@@ -11,16 +11,25 @@ namespace WickedSick.Server.XamlParser
     {
         static readonly ILog Log = LogManager.GetLogger(typeof(PropertyDescription));
 
-        private static IDictionary<string, PropertyDescription> _dependencyProperties = new Dictionary<string, PropertyDescription>();
+        private static Dictionary<Type, PropertyDescription> _ContentProperty = new Dictionary<Type, PropertyDescription>();
+        private static Dictionary<Type, Dictionary<string, PropertyDescription>> _GroupedDependencyProperties = new Dictionary<Type, Dictionary<string, PropertyDescription>>();
 
         public static PropertyDescription Register(string name, Type type, Type ownerType, bool isContent = false)
         {
-            string key = string.Format("{0}.{1}", ownerType.Name, name);
-            PropertyDescription dp = new PropertyDescription(name, type, isContent);
-            if (_dependencyProperties.Keys.Contains(key))
-                throw new ArgumentException(string.Format("The DependencyProperty ({0}) has already been registered.", key));
-            //TODO: check and make sure more than one content property is not registered
-            _dependencyProperties.Add(key, dp);
+            var groupDict = _GroupedDependencyProperties.ContainsKey(ownerType) ? _GroupedDependencyProperties[ownerType] : null;
+            if (groupDict == null)
+                _GroupedDependencyProperties[ownerType] = groupDict = new Dictionary<string, PropertyDescription>();
+
+            if (groupDict.ContainsKey(name))
+                throw new ArgumentException(string.Format("The DependencyProperty ({0}.{1}) has already been registered.", ownerType.Name, name));
+
+            if (isContent && _ContentProperty.ContainsKey(ownerType))
+                throw new ArgumentException(string.Format("There cannot be more than one content property on a DependencyObject ({0})", ownerType.Name));
+
+            var dp = new PropertyDescription(name, type, isContent);
+            groupDict[name] = dp;
+            if (isContent)
+                _ContentProperty[ownerType] = dp;
             return dp;
         }
 
@@ -30,10 +39,12 @@ namespace WickedSick.Server.XamlParser
             while (checkType != null)
             {
                 PropertyHelper.EnsurePropertyRegistered(checkType);
-
-                string key = string.Format("{0}.{1}", checkType.Name, name);
-                if (_dependencyProperties.Keys.Contains(key))
-                    return _dependencyProperties[key];
+                var groupDict = _GroupedDependencyProperties.ContainsKey(checkType) ? _GroupedDependencyProperties[checkType] : null;
+                if (groupDict != null)
+                {
+                    if (groupDict.ContainsKey(name))
+                        return groupDict[name];
+                }
 
                 checkType = checkType.BaseType;
             }
@@ -43,19 +54,15 @@ namespace WickedSick.Server.XamlParser
 
         public static PropertyDescription GetContent(Type ownerType)
         {
+            if (_ContentProperty.ContainsKey(ownerType))
+                return _ContentProperty[ownerType];
+
             Type checkType = ownerType;
             while (checkType != null)
             {
                 PropertyHelper.EnsurePropertyRegistered(checkType);
-
-                var pd = _dependencyProperties
-                    .ToList()
-                    .Where(kvp => kvp.Key.StartsWith(checkType.Name + ".") && kvp.Value.IsContent)
-                    .Select(kvp => kvp.Value)
-                    .FirstOrDefault();
-                if (pd != null)
-                    return pd;
-
+                if (_ContentProperty.ContainsKey(checkType))
+                    return _ContentProperty[checkType];
                 checkType = checkType.BaseType;
             }
             return null;
--- Source/WickedSick.Server.XamlParser/PropertyHelper.cs
+++ Source/WickedSick.Server.XamlParser/PropertyHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -6,12 +7,17 @@ namespace WickedSick.Server.XamlParser
 {
     public static class PropertyHelper
     {
+        private static List<Type> _TypeRegistered = new List<Type>();
+
         public static void EnsurePropertyRegistered(Type type)
         {
+            if (_TypeRegistered.Contains(type))
+                return;
             type.GetMembers(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public)
                 .Concat(type.GetMembers(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic))
                 .OfType<FieldInfo>().Where(fi => fi.FieldType == typeof(PropertyDescription) || fi.FieldType == typeof(AttachedPropertyDescription)).ToList()
                 .ForEach(fi => fi.GetValue(null));
+            _TypeRegistered.Add(type);
         }
     }
 }
