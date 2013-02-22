51bb6c748bfffbd206fd926d88bf97e7420de0c4
BSick7 <brad.sickles@gmail.com>
2013-02-22 11:04:27 -0500

    Added test for refequals.

--- Source/WickedSick.Fayde.Client.Engine/Javascript/tsc-all.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET o=temp.txt
-
-echo --module amd >> %o%
-for /r . %%g in (*.ts) do echo %%g >> %o%
-
-"C:\Program Files (x86)\Microsoft SDKs\TypeScript\0.8.0.0\tsc.exe" @"%CD%\%o%"
-
-DEL %o%
