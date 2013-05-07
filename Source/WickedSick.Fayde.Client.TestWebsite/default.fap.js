7c988b33c5ae6af5643006f7618097ca40785209
BSick7 <brad.sickles@gmail.com>
2013-05-06 20:07:21 -0400

    Temporarily converting default test code behind.

--- Source/WickedSick.Fayde.Client.TestWebsite/default.fap.js
+++ Source/WickedSick.Fayde.Client.TestWebsite/default.fap.js
@@ -1,15 +1,19 @@
-﻿(function (namespace) {
-    var app = Nullstone.Create("app", App);
-
-    app.Instance.Init = function () {
-        this.Init$App();
-        this.Loaded.Subscribe(this.OnLoaded, this);
-    };
-
-    app.Instance.OnLoaded = function (sender, e) {
-
-    };
-
-    Nullstone.FinishCreate(app);
-    namespace.App = app;
-})(Nullstone.Namespace("TestWebsite"));
+﻿var __extends = this.__extends || function (d, b) {
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var TestWebsite;
+(function (TestWebsite) {
+    var Application = (function (_super) {
+        __extends(Application, _super);
+        function Application() {
+            _super.call(this);
+            this.Loaded.Subscribe(this.OnLoaded, this);
+        }
+        Application.prototype.OnLoaded = function (sender, e) {
+        };
+        return Application;
+    })(App);
+    TestWebsite.Application = Application;
+})(TestWebsite || (TestWebsite = {}));
