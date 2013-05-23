07073e09556ce3e8899f3a100141f8fe7f87bb49
BSick7 <brad.sickles@gmail.com>
2013-05-23 15:08:22 -0400

    Removing auto-gen *.js files from source control.

--- Source/WickedSick.Fayde.Client.Engine/Typescript/Runtime/EventArgs.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../Runtime/Nullstone.ts" />
-/// CODE
-var EventArgs = (function () {
-    function EventArgs() { }
-    EventArgs.Empty = new EventArgs();
-    return EventArgs;
-})();
-Nullstone.RegisterType(EventArgs, "EventArgs");
-//@ sourceMappingURL=EventArgs.js.map
