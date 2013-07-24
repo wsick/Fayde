617fdc51707e54ebfcc8485538ecade56d228c07
BSick7 <brad.sickles@gmail.com>
2013-07-24 15:58:12 -0400

    Install powershell script show not fail now on upgrade.  Also ticking version for added server property registrations.

--- Packages/SourceFiles/tools/install.ps1
+++ Packages/SourceFiles/tools/install.ps1
@@ -6,25 +6,28 @@ Set-WebAppProjectProperty $project "IISUrl" ("http://localhost/" + $project.Proj
 Set-WebAppProjectProperty $project "DebugStartAction" 1
 Set-WebAppProjectProperty $project "StartPageUrl" "default.fap"
 
-
 #Set up default.fap
 Set-ProjectItemType $project "default.fap.ts" "TypeScriptCompile"
 
 $rootitem = Get-ProjectItem $project "default.fap"
-$tsitem = Get-ProjectItem $project "default.fap.ts"
-Add-ProjectItemChild $project $rootitem $tsitem
+IF ($rootitem -ne $null) {
+    $tsitem = Get-ProjectItem $project "default.fap.ts"
+    Add-ProjectItemChild $project $rootitem $tsitem
 
-$jsitem = Get-ProjectItem $project "default.fap.js"
-Add-ProjectItemChild $project $tsitem $jsitem
+    $jsitem = Get-ProjectItem $project "default.fap.js"
+    Add-ProjectItemChild $project $tsitem $jsitem
+}
 
 #Set up default.fayde
 Set-ProjectItemType $project "default.fayde.ts" "TypeScriptCompile"
 
 $rootitem = Get-ProjectItem $project "default.fayde"
-$tsitem = Get-ProjectItem $project "default.fayde.ts"
-Add-ProjectItemChild $project $rootitem $tsitem
+IF ($rootitem -ne $null) {
+    $tsitem = Get-ProjectItem $project "default.fayde.ts"
+    Add-ProjectItemChild $project $rootitem $tsitem
 
-$jsitem = Get-ProjectItem $project "default.fayde.js"
-Add-ProjectItemChild $project $tsitem $jsitem
+    $jsitem = Get-ProjectItem $project "default.fayde.js"
+    Add-ProjectItemChild $project $tsitem $jsitem
+}
 
 $project.Save()
