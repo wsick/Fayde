3320f94d899eead4153c8547b792556c14d4b379
ssickles <scottsickles@gmail.com>
2013-08-07 08:42:19 -0400

    - added missing images to the NflDraft demo app
    - started work on the Timeline view in Thea

---
+++ Tools/WickedSick.Thea/Resources/PillColorValueConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace WickedSick.Thea.Resources
+{
+    class PillColorValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
