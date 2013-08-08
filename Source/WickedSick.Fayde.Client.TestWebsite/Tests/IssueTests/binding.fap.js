c62218da350a80a89a5b8093b41c28be29ecde7e
BSick7 <brad.sickles@gmail.com>
2013-08-08 10:19:09 -0400

    Added test for cross-element binding where selected item of a ListBox is bounds 2way to the view model and that property is used as a source of another binding.  ObservableObject didn't have interface registration.

---
+++ Source/WickedSick.Fayde.Client.TestWebsite/Tests/IssueTests/binding.fap.js
@@ -0,0 +1,39 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var Tests;
+(function (Tests) {
+    /// <reference path="../../../jsbin/Fayde.d.ts" />
+    (function (IssueTests) {
+        var Application = (function (_super) {
+            __extends(Application, _super);
+            function Application() {
+                _super.call(this);
+                this.Loaded.Subscribe(this.OnLoaded, this);
+            }
+            Application.prototype.OnLoaded = function (sender, e) {
+                this.RootVisual.DataContext = new TestViewModel();
+            };
+            return Application;
+        })(App);
+        IssueTests.Application = Application;
+        Nullstone.RegisterType(Application, "Application");
+
+        var TestViewModel = (function (_super) {
+            __extends(TestViewModel, _super);
+            function TestViewModel() {
+                _super.apply(this, arguments);
+                this.AllItems = ["Item1", "Item2", "Item3"];
+            }
+            TestViewModel.ctor = (function () {
+                Fayde.MVVM.NotifyProperties(TestViewModel, ["SelectedItem"]);
+            })();
+            return TestViewModel;
+        })(Fayde.MVVM.ViewModelBase);
+        IssueTests.TestViewModel = TestViewModel;
+    })(Tests.IssueTests || (Tests.IssueTests = {}));
+    var IssueTests = Tests.IssueTests;
+})(Tests || (Tests = {}));
---
+++ Source/WickedSick.Fayde.Client.TestWebsite/Tests/IssueTests/binding.fap.ts
@@ -0,0 +1,22 @@
+﻿/// <reference path="../../../jsbin/Fayde.d.ts" />
+
+module Tests.IssueTests {
+    export class Application extends App {
+        constructor() {
+            super();
+            this.Loaded.Subscribe(this.OnLoaded, this);
+        }
+        OnLoaded(sender, e: EventArgs) {
+            this.RootVisual.DataContext = new TestViewModel();
+        }
+    }
+    Nullstone.RegisterType(Application, "Application");
+
+    export class TestViewModel extends Fayde.MVVM.ViewModelBase {
+        AllItems: string[] = ["Item1", "Item2", "Item3"];
+        SelectedItem: string;
+        private static ctor = (() => {
+            Fayde.MVVM.NotifyProperties(TestViewModel, ["SelectedItem"]);
+        })();
+    }
+}
