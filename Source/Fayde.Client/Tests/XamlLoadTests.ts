64401eba717d7670d443d6d78bc4731126c12fab
BSick7 <brad.sickles@gmail.com>
2013-08-20 11:44:08 -0400

    Added ControlTemplate and ItemsPanelTemplate xaml load test.

--- Source/Fayde.Client/Tests/XamlLoadTests.ts
+++ Source/Fayde.Client/Tests/XamlLoadTests.ts
@@ -86,10 +86,70 @@ test("DataTemplate", () => {
     try {
         dt = <Fayde.DataTemplate>Fayde.Xaml.Load(xaml);
     } catch (err) {
-        ok(false, "Loading a datatemplate should not error. " + err.toString());
+        ok(false, "Loading a DataTemplate should not error. " + err.toString());
     }
     strictEqual((<any>dt).constructor, Fayde.DataTemplate, "Resulting object should be a DataTemplate.");
 
     var visual = dt.GetVisualTree(null);
+    strictEqual((<any>visual).constructor, Fayde.Controls.Grid, "Root visual from created visual tree should be a Grid.");
+});
+
+test("ControlTemplate", () => {
+    var xaml = "<ControlTemplate xmlns=\"http://schemas.wsick.com/fayde\" xmlns:x=\"http://schemas.wsick.com/fayde/x\">"
+        + "<Grid></Grid>"
+        + "</ControlTemplate>";
+
+    var ct: Fayde.Controls.ControlTemplate;
+    try {
+        ct = <Fayde.Controls.ControlTemplate>Fayde.Xaml.Load(xaml);
+        ok(false, "Loading a ControlTemplate should error if no TargetType is specified.");
+    } catch (err) {
+        ok(err instanceof XamlParseException, "Loading a ControlTemplate should error if no TargetType is specified.");
+    }
+    
+    xaml = "<ControlTemplate xmlns=\"http://schemas.wsick.com/fayde\" xmlns:x=\"http://schemas.wsick.com/fayde/x\" TargetType=\"Control\">"
+        + "<Grid></Grid>"
+        + "</ControlTemplate>";
+    
+    try {
+        ct = <Fayde.Controls.ControlTemplate>Fayde.Xaml.Load(xaml);
+        ok(true, "Loading a ControlTemplate with a TargetType should succeed.");
+    } catch (err) {
+        ok(false, "Loading a ControlTemplate with a TargetType should succeed.");
+    }
+
+    strictEqual((<any>ct).constructor, Fayde.Controls.ControlTemplate, "Template should be a ControlTemplate.");
+    strictEqual(ct.TargetType, Fayde.Controls.Control, "ControlTemplate.TargetType should be Control.");
+    
+    var visual = ct.GetVisualTree(null);
+    strictEqual((<any>visual).constructor, Fayde.Controls.Grid, "Root visual from created visual tree should be a Grid.");
+});
+
+test("ItemsPanelTemplate", () => {
+    var xaml = "<ItemsPanelTemplate xmlns=\"http://schemas.wsick.com/fayde\" xmlns:x=\"http://schemas.wsick.com/fayde/x\">"
+        + "<Border />"
+        + "</ItemsPanelTemplate>";
+    
+    var ipt: Fayde.Controls.ItemsPanelTemplate;
+    try {
+        ipt = <Fayde.Controls.ItemsPanelTemplate>Fayde.Xaml.Load(xaml);
+        var tempPanel = ipt.GetVisualTree(null);
+        ok(false, "Getting the visual tree for an ItemsPanelTemplate with a non-Panel root visual should error.");
+    } catch (err) {
+        ok(err instanceof XamlParseException, "Getting the visual tree for an ItemsPanelTemplate with a non-Panel root visual should error.");
+    }
+
+    xaml = "<ItemsPanelTemplate xmlns=\"http://schemas.wsick.com/fayde\" xmlns:x=\"http://schemas.wsick.com/fayde/x\">"
+        + "<Grid></Grid>"
+        + "</ItemsPanelTemplate>";
+
+    try {
+        ipt = <Fayde.Controls.ItemsPanelTemplate>Fayde.Xaml.Load(xaml);
+    } catch (err) {
+        ok(false, "Loading a ItemsPanelTemplate should not error. " + err.toString());
+    }
+    strictEqual((<any>ipt).constructor, Fayde.Controls.ItemsPanelTemplate, "Resulting object should be a ItemsPanelTemplate.");
+
+    var visual = ipt.GetVisualTree(null);
     strictEqual((<any>visual).constructor, Fayde.Controls.Grid, "Root visual from created visual tree should be a Grid.");
 });
