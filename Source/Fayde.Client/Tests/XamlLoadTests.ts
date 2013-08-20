54c312842fd8237a8720149ede9ed012ae27cc52
BSick7 <brad.sickles@gmail.com>
2013-08-19 21:09:58 -0400

    Fixed test message.

--- Source/Fayde.Client/Tests/XamlLoadTests.ts
+++ Source/Fayde.Client/Tests/XamlLoadTests.ts
@@ -45,10 +45,10 @@ test("Resource Dictionary", () => {
     var resources = root.Resources;
     ok(resources.ContainsKey("SomeThickness"), "Resources should contain a resource with a key 'SomeThickness'");
     var thickness = <Thickness>resources.Get("SomeThickness");
-    strictEqual(thickness.Left, 1, "Thickness.Left must equal 1 and not \"1\"");
-    strictEqual(thickness.Top, 2, "Thickness.Top must equal 2 and not \"2\"");
-    strictEqual(thickness.Right, 3, "Thickness.Right must equal 3 and not \"3\"");
-    strictEqual(thickness.Bottom, 4, "Thickness.Bottom must equal 4 and not \"4\"");
+    strictEqual(thickness.Left, 1, "Thickness.Left must equal 1 and not \"1\".");
+    strictEqual(thickness.Top, 2, "Thickness.Top must equal 2 and not \"2\".");
+    strictEqual(thickness.Right, 3, "Thickness.Right must equal 3 and not \"3\".");
+    strictEqual(thickness.Bottom, 4, "Thickness.Bottom must equal 4 and not \"4\".");
 });
 
 test("Style", () => {
