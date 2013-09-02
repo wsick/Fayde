491a9cd6f734d9c4ff04e80e29b96500fee66f23
BSick7 <brad.sickles@gmail.com>
2013-09-02 17:40:57 -0400

    Added AgeValueConverter to nfl draft demo.

---
+++ Demos/NflDraft/Resources/AgeValueConverter.js
@@ -0,0 +1,32 @@
+var NflDraft;
+(function (NflDraft) {
+    /// <reference path="../scripts/Fayde.d.ts" />
+    (function (Resources) {
+        var AgeValueConverter = (function () {
+            function AgeValueConverter() {
+            }
+            AgeValueConverter.prototype.Convert = function (value, targetType, parameter, culture) {
+                var today = new Date();
+                var age = today.getFullYear() - value.getFullYear();
+                if (today.getMonth() < value.getMonth())
+                    age--;
+                if (value.getMonth() - 1 == today.getMonth() && today.getDate() < value.getDate())
+                    age--;
+                return age;
+            };
+            AgeValueConverter.prototype.ConvertBack = function (value, targetType, parameter, culture) {
+                throw NotImplemented("This ValueConverter only does conversion. It cannot convert back.");
+            };
+            return AgeValueConverter;
+        })();
+        Resources.AgeValueConverter = AgeValueConverter;
+        Fayde.RegisterType(AgeValueConverter, {
+            Name: "AgeValueConverter",
+            Namespace: "NflDraft.Resources",
+            XmlNamespace: "folder:Resources",
+            Interfaces: [Fayde.Data.IValueConverter_]
+        });
+    })(NflDraft.Resources || (NflDraft.Resources = {}));
+    var Resources = NflDraft.Resources;
+})(NflDraft || (NflDraft = {}));
+//# sourceMappingURL=AgeValueConverter.js.map
