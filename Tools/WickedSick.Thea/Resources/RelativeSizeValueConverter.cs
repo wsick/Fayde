21138e15ecba2be25f4f39cbee3a0893d224f015
BSick7 <brad.sickles@gmail.com>
2013-09-25 09:40:38 -0400

    Fixed botched merge for Thea.

---
+++ Tools/WickedSick.Thea/Resources/RelativeSizeValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace WickedSick.Thea.Resources
+{
+    class RelativeSizeValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double amount = double.Parse(values[0].ToString());
+            double total = double.Parse(values[1].ToString());
+            double totalSize = double.Parse(values[2].ToString());
+            return (amount / total) * totalSize;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
