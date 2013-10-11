7cd534818f5b778246d1db6a3b29fc42d62deacf
BSick7 <brad.sickles@gmail.com>
2013-10-11 14:50:03 -0400

    Added code to stop browser context menu if right-click is handled.

--- Source/Fayde.Client/Typescript/Input/MouseInterop.ts
+++ Source/Fayde.Client/Typescript/Input/MouseInterop.ts
@@ -1,6 +1,7 @@
 module Fayde.Input {
     var isIE = navigator.appName === "Microsoft Internet Explorer";
     var isNetscape = navigator.appName === "Netscape";
+    var isContextMenuDisabled = false;
     export class MouseInterop {
         static IsLeftButton(button: number): boolean {
             return button === 1;
@@ -8,6 +9,16 @@ module Fayde.Input {
         static IsRightButton(button: number): boolean {
             if (isNetscape) return button === 3;
             return button === 2;
+        }
+
+        static DisableBrowserContextMenu() {
+            isContextMenuDisabled = true;
+        }
+        static get IsBrowserContextMenuDisabled(): boolean {
+            if (!isContextMenuDisabled)
+                return false;
+            isContextMenuDisabled = false;
+            return true;
         }
     }
 }
