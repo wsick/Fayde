30d770b8822c405108dbc99f8b8c1cdd5aa1360c
BSick7 <brad.sickles@gmail.com>
2013-11-16 08:38:28 -0500

    Fixed arc to bounds calculations.

--- Source/Fayde.Client/Path/ArcTo.ts
+++ Source/Fayde.Client/Path/ArcTo.ts
@@ -1,4 +1,10 @@
+function radToDegrees(rad) {
+    return rad * 180 / Math.PI;
+}
+
 module Fayde.Path {
+    var EPSILON = 1e-10;
+
     export interface IArcTo extends IPathEntry {
         cpx: number;
         cpy: number;
@@ -13,41 +19,27 @@ module Fayde.Path {
         sy: number;
         ex: number;
         ey: number;
+        cc: boolean;
     }
     export function ArcTo(cpx: number, cpy: number, x: number, y: number, radius: number): IArcTo {
         function createAutoArc(prevX: number, prevY: number): IAutoArc {
-            var v1x = cpx - prevX;
-            var v1y = cpy - prevY;
-            var v1_len = Math.sqrt(v1x * v1x + v1y * v1y);
-            var v2x = x - cpx;
-            var v2y = y - cpy;
-
-            var theta_outer1 = Math.atan2(Math.abs(v1y), Math.abs(v1x));
-            var theta_outer2 = Math.atan2(Math.abs(v2y), Math.abs(v2x));
-            var inner_theta = Math.PI - theta_outer1 - theta_outer2;
-
-            //distance to center of imaginary circle
-            var h = radius / Math.sin(inner_theta / 2);
-            //cx, cy -> center of imaginary circle
-            var cx = cpx + h * Math.cos(inner_theta / 2 + theta_outer2);
-            var cy = cpy + h * Math.sin(inner_theta / 2 + theta_outer2);
-            //distance from cp -> tangent points on imaginary circle
-            var a = radius / Math.tan(inner_theta / 2);
-            //tangent point at start of arc
-            var x1 = cpx + a * Math.cos(theta_outer2 + inner_theta);
-            var y1 = cpy + a * Math.sin(theta_outer2 + inner_theta);
-            //tangent point at end of arc
-            var x2 = cpx + a * Math.cos(theta_outer2);
-            var y2 = cpy + a * Math.sin(theta_outer2);
+            var v1 = [cpx - prevX, cpy - prevY];
+            var v2 = [x - cpx, y - cpy];
+            var inner_theta = angleBetweenVectors(v1, v2);
+            //find 2 points tangent to imaginary circle along guide lines
+            var a = getTangentPoint(inner_theta, radius, [prevX, prevY], v1, true);
+            var b = getTangentPoint(inner_theta, radius, [cpx, cpy], v2, false);
+            //find center point
+            var c = getPerpendicularIntersections(a, v1, b, v2);
 
             return {
-                cx: cx,
-                cy: cy,
-                sx: x1,
-                sy: x2,
-                ex: y1,
-                ey: y2,
-                cc: true
+                cx: c[0],
+                cy: c[1],
+                sx: a[0],
+                sy: a[1],
+                ex: b[0],
+                ey: b[1],
+                cc: isCounterClockwise([-v1[0], -v1[1]], v2, inner_theta)
             };
         }
 
@@ -65,20 +57,21 @@ module Fayde.Path {
                 console.warn("[NOT IMPLEMENTED] Measure ArcTo");
                 
                 var aa = createAutoArc(prevX, prevY);
+                console.log(aa.cc);
                 
-                box.l = Math.min(box.l, aa.sx);
-                box.r = Math.max(box.r, aa.sy);
-                box.t = Math.min(box.t, aa.sx);
-                box.b = Math.max(box.b, aa.sy);
+                box.l = Math.min(box.l, prevX, aa.sx, aa.ex);
+                box.r = Math.max(box.r, prevX, aa.sx, aa.ex);
+                box.t = Math.min(box.t, prevY, aa.sy, aa.ey);
+                box.b = Math.max(box.b, prevY, aa.sy, aa.ey);
 
-                var pts = getArcPoints(aa, radius, true);
-                if (isNaN(pts.l))
+                var pts = getArcPoints(aa, radius, aa.cc);
+                if (!isNaN(pts.l))
                     box.l = Math.min(box.l, pts.l);
-                if (isNaN(pts.r))
+                if (!isNaN(pts.r))
                     box.r = Math.max(box.r, pts.r);
-                if (isNaN(pts.t))
+                if (!isNaN(pts.t))
                     box.t = Math.min(box.t, pts.t);
-                if (isNaN(pts.b))
+                if (!isNaN(pts.b))
                     box.b = Math.max(box.b, pts.b);
 
                 //Still need to expand box for line (prevX,prevY) --> (sx,sy)
@@ -88,21 +81,21 @@ module Fayde.Path {
                 
                 var aa = createAutoArc(prevX, prevY);
                 
-                box.l = Math.min(box.l, aa.sx);
-                box.r = Math.max(box.r, aa.sy);
-                box.t = Math.min(box.t, aa.sx);
-                box.b = Math.max(box.b, aa.sy);
+                box.l = Math.min(box.l, prevX, aa.sx, aa.ex);
+                box.r = Math.max(box.r, prevX, aa.sx, aa.ex);
+                box.t = Math.min(box.t, prevY, aa.sy, aa.ey);
+                box.b = Math.max(box.b, prevY, aa.sy, aa.ey);
 
                 var hs = pars.thickness / 2.0;
 
                 var pts = getArcPoints(aa, radius, true);
-                if (isNaN(pts.l))
+                if (!isNaN(pts.l))
                     box.l = Math.min(box.l, pts.l - hs);
-                if (isNaN(pts.r))
+                if (!isNaN(pts.r))
                     box.r = Math.max(box.r, pts.r + hs);
-                if (isNaN(pts.t))
+                if (!isNaN(pts.t))
                     box.t = Math.min(box.t, pts.t - hs);
-                if (isNaN(pts.b))
+                if (!isNaN(pts.b))
                     box.b = Math.max(box.b, pts.b + hs);
 
                 //Still need to expand box for line (prevX,prevY) --> (sx,sy)
@@ -159,5 +152,60 @@ module Fayde.Path {
         if (n < 0 && !cc)
             return true;
         return false;
+    }
+
+    function angleBetweenVectors(u: number[], v: number[]): number {
+        var num = u[0] * v[0] + u[1] * v[1];
+        var den = Math.sqrt(u[0] * u[0] + u[1] * u[1]) * Math.sqrt(v[0] * v[0] + v[1] * v[1]);
+        return Math.PI - Math.acos(num / den);
+    }
+    function getTangentPoint(theta: number, radius: number, s: number[], d: number[], invert: boolean): number[] {
+        var len = Math.sqrt(d[0] * d[0] + d[1] * d[1]);
+        var f = radius / Math.tan(theta / 2);
+        var t = f / len;
+        if (invert)
+            t = 1 - t;
+        return [s[0] + t * d[0], s[1] + t * d[1]];
+    }
+    function getPerpendicularIntersections(s1: number[], d1: number[], s2: number[], d2: number[]): number[] {
+        var x1 = s1[0];
+        var y1 = s1[1];
+        var x2 = s1[0] - d1[1];
+        var y2 = s1[1] + d1[0];
+
+        var x3 = s2[0];
+        var y3 = s2[1];
+        var x4 = s2[0] - d2[1];
+        var y4 = s2[1] + d2[0];
+
+        var det = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+        if (det === 0)
+            return null;
+
+        var xn = ((x1 * y2 - y1 * x2) * (x3 - x4)) - ((x1 - x2) * (x3 * y4 - y3 * x4));
+        var yn = ((x1 * y2 - y1 * x2) * (y3 - y4)) - ((y1 - y2) * (x3 * y4 - y3 * x4));
+        return [xn / det, yn / det];
+    }
+
+    function isCounterClockwise(v1: number[], v2: number[], theta: number) {
+        var nv1 = normalizeVector(v1);
+        var nv2 = normalizeVector(v2);
+        var ccv1 = rotateVector(nv1, theta);
+        var nx = Math.abs(ccv1[0] - nv2[0]);
+        var ny = Math.abs(ccv1[1] - nv2[1]);
+        var tau = angleBetweenVectors(ccv1, nv2);
+        return nx < EPSILON
+            && ny < EPSILON;
+    }
+    function normalizeVector(v: number[]): number[] {
+        var len = Math.sqrt(v[0] * v[0] + v[1] * v[1]);
+        return [v[0] / len, v[1] / len];
+    }
+    function rotateVector(v: number[], angle: number): number[] {
+        var c = Math.cos(angle);
+        var s = Math.sin(angle);
+        var x = v[0];
+        var y = v[1];
+        return [x * c - y * s, x * s + y * c];
     }
 }
