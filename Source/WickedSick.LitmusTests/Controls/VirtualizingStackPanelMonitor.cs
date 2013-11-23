2bd94b5fe8c4f38e7a9bc19872d65dad5cfd5155
BSick7 <brad.sickles@gmail.com>
2013-11-23 13:08:21 -0500

    Added tests to verify VirtualizingStackPanel behavior.

---
+++ Source/WickedSick.LitmusTests/Controls/VirtualizingStackPanelMonitor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace WickedSick.LitmusTests.Controls
+{
+    public class VirtualizingStackPanelMonitor : VirtualizingStackPanel
+    {
+        protected override Size MeasureOverride(Size constraint)
+        {
+            Debug.WriteLine("MeasureOverride (VirtualizingStackPanel): " + constraint.ToString());
+            return base.MeasureOverride(constraint);
+        }
+    }
+}
