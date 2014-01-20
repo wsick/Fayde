d6725a17b764105103ec1a21dc1a07b8cee0f57c
BSick7 <brad.sickles@gmail.com>
2014-01-20 14:32:33 -0500

    Repaired broken issue tests from changes.

--- Source/Fayde.TestSite/Tests/IssueTests/DoubleListBoxViewModel.js
+++ Source/Fayde.TestSite/Tests/IssueTests/DoubleListBoxViewModel.js
@@ -5,51 +5,47 @@ var __extends = this.__extends || function (d, b) {
     __.prototype = b.prototype;
     d.prototype = new __();
 };
-var Tests;
-(function (Tests) {
-    (function (IssueTests) {
-        var RelayCommand = Fayde.MVVM.RelayCommand;
+define(["require", "exports"], function(require, exports) {
+    var RelayCommand = Fayde.MVVM.RelayCommand;
 
-        var DoubleListBoxViewModel = (function (_super) {
-            __extends(DoubleListBoxViewModel, _super);
-            function DoubleListBoxViewModel() {
-                var _this = this;
-                _super.call(this);
-                this.AllItems = new Fayde.Collections.ObservableCollection();
-                for (var i = 0; i < 6; i++) {
-                    var ni = {
-                        SubItems: new Fayde.Collections.ObservableCollection()
-                    };
-                    ni.SubItems.AddRange([1, 2, 3, 4, 5]);
-                    this.AllItems.Add(ni);
-                }
-                this.AddItemCommand = new RelayCommand(function () {
-                    return _this.Add();
-                });
-                this.RemoveFirstItemCommand = new RelayCommand(function () {
-                    return _this.RemoveFirst();
-                });
+    var DoubleListBoxViewModel = (function (_super) {
+        __extends(DoubleListBoxViewModel, _super);
+        function DoubleListBoxViewModel() {
+            var _this = this;
+            _super.call(this);
+            this.AllItems = new Fayde.Collections.ObservableCollection();
+            for (var i = 0; i < 6; i++) {
+                var ni = {
+                    SubItems: new Fayde.Collections.ObservableCollection()
+                };
+                ni.SubItems.AddRange([1, 2, 3, 4, 5]);
+                this.AllItems.Add(ni);
             }
-            DoubleListBoxViewModel.prototype.Add = function () {
-                if (this.AllItems.Count < 1)
-                    return;
-                var first = this.AllItems.GetValueAt(0);
-                first.SubItems.Add(first.SubItems.Count);
-            };
-            DoubleListBoxViewModel.prototype.RemoveFirst = function () {
-                if (this.AllItems.Count < 1)
-                    return;
-                var first = this.AllItems.GetValueAt(0);
-                if (first.SubItems.Count < 1)
-                    return;
-                first.SubItems.RemoveAt(0);
-                if (first.SubItems.Count < 1)
-                    this.AllItems.RemoveAt(0);
-            };
-            return DoubleListBoxViewModel;
-        })(Fayde.MVVM.ViewModelBase);
-        IssueTests.DoubleListBoxViewModel = DoubleListBoxViewModel;
-        Fayde.RegisterType(DoubleListBoxViewModel, "Tests.IssueTests", "folder:Tests/IssueTests");
-    })(Tests.IssueTests || (Tests.IssueTests = {}));
-    var IssueTests = Tests.IssueTests;
-})(Tests || (Tests = {}));
+            this.AddItemCommand = new RelayCommand(function () {
+                return _this.Add();
+            });
+            this.RemoveFirstItemCommand = new RelayCommand(function () {
+                return _this.RemoveFirst();
+            });
+        }
+        DoubleListBoxViewModel.prototype.Add = function () {
+            if (this.AllItems.Count < 1)
+                return;
+            var first = this.AllItems.GetValueAt(0);
+            first.SubItems.Add(first.SubItems.Count);
+        };
+        DoubleListBoxViewModel.prototype.RemoveFirst = function () {
+            if (this.AllItems.Count < 1)
+                return;
+            var first = this.AllItems.GetValueAt(0);
+            if (first.SubItems.Count < 1)
+                return;
+            first.SubItems.RemoveAt(0);
+            if (first.SubItems.Count < 1)
+                this.AllItems.RemoveAt(0);
+        };
+        return DoubleListBoxViewModel;
+    })(Fayde.MVVM.ViewModelBase);
+    
+    return DoubleListBoxViewModel;
+});
