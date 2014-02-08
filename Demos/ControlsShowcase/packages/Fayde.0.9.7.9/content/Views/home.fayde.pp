5796e4e432ec05aa0e8c1befa15abea83c15f12c
BSick7 <brad.sickles@gmail.com>
2014-02-07 21:08:57 -0500

    Updating fayde package. Ignoring packages dir.

--- Demos/ControlsShowcase/packages/Fayde.0.9.7.9/content/Views/home.fayde.pp
+++
@@ -1,7 +0,0 @@
-<Page xmlns="http://schemas.wsick.com/fayde"
-      xmlns:x="http://schemas.wsick.com/fayde/x"
-      Title="Home">
-    <Grid x:Name="LayoutRoot">
-		<TextBlock Text="Hello World!" />
-    </Grid>
-</Page>
