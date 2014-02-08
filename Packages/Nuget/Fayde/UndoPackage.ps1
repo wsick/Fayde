73e93d95d40cbd146d48a5883435afda2386c23d
BSick7 <brad.sickles@gmail.com>
2014-02-08 09:45:40 -0500

    Automated way of destroying created package that didn't pass tests. Must be done before publishing.

---
+++ Packages/Nuget/Fayde/UndoPackage.ps1
@@ -0,0 +1,14 @@
+$vpath = $PWD.Path + "\version.txt"
+$versionstring = Get-Content $vpath
+$tokens = $versionstring.Split(".")
+$major = $tokens.Get(0)
+$minor = $tokens.Get(1)
+$build = $tokens.Get(2)
+$revision = $tokens.Get(3)
+
+$oldrevision = ([int]$revision - 1).ToString()
+$newversion = "$major.$minor.$build.$oldrevision"
+Set-Content -Value $newversion $vpath
+
+$oldpkg = $PWD.Path + "\..\Fayde.$major.$minor.$build.$revision.nupkg"
+ri $oldpkg
