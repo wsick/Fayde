ffff698ec75ac0449487457556e76e92748b3f4d
BSick7 <brad.sickles@gmail.com>
2014-02-14 10:26:32 -0500

    Added ContentControl.ContentUri with the ability to load remote xaml resources.

--- Source/Fayde.Client/Controls/ContentControl.ts
+++ Source/Fayde.Client/Controls/ContentControl.ts
@@ -29,15 +29,43 @@ module Fayde.Controls {
         CreateNode(): ContentControlNode { return new ContentControlNode(this); }
 
         _ContentSetsParent: boolean = true;
+
         static ContentProperty: DependencyProperty = DependencyProperty.Register("Content", () => Object, ContentControl, undefined, (d, args) => (<ContentControl>d).OnContentChanged(args.OldValue, args.NewValue));
-        static ContentTemplateProperty = DependencyProperty.Register("ContentTemplate", () => DataTemplate, ContentControl, undefined, (d, args) => (<ContentControl>d).OnContentTemplateChanged(args.OldValue, args.NewValue));
         Content: any;
+        OnContentChanged(oldContent: any, newContent: any) { }
+
+        static ContentTemplateProperty = DependencyProperty.Register("ContentTemplate", () => DataTemplate, ContentControl, undefined, (d, args) => (<ContentControl>d).OnContentTemplateChanged(args.OldValue, args.NewValue));
         ContentTemplate: DataTemplate;
+        OnContentTemplateChanged(oldContentTemplate: DataTemplate, newContentTemplate: DataTemplate) { }
+
+        static ContentUriProperty = DependencyProperty.Register("ContentUri", () => Uri, ContentControl, undefined, (d, args) => (<ContentControl>d).OnContentUriPropertyChanged(args));
+        ContentUri: Uri;
+        private OnContentUriPropertyChanged(args: DependencyPropertyChangedEventArgs) {
+            var oldUri: Uri;
+            if (args.OldValue instanceof Uri) {
+                this.Content = undefined;
+                oldUri = <Uri>args.OldValue;
+            }
+            var newUri: Uri;
+            if (args.NewValue instanceof Uri) {
+                newUri = <Uri>args.NewValue;
+                Xaml.XamlDocument.Resolve(newUri)
+                    .success(xd => this._OnLoadedUri(xd))
+                    .error(err => this._OnErroredUri(err, newUri));
+            }
+            this.OnContentUriChanged(oldUri, newUri);
+        }
+        OnContentUriChanged(oldSourceUri: Uri, newSourceUri: Uri) { }
 
         static Annotations = { ContentProperty: ContentControl.ContentProperty }
 
-        OnContentChanged(oldContent: any, newContent: any) { }
-        OnContentTemplateChanged(oldContentTemplate: DataTemplate, newContentTemplate: DataTemplate) { }
+        private _OnLoadedUri(xd: Xaml.XamlDocument) {
+            this.Content = Xaml.Load(xd.Document);
+        }
+        private _OnErroredUri(err: any, src: Uri) {
+            console.warn("Error resolving XamlResource: '" + src.toString() + "'.")
+            //TODO: Set content to error message?
+        }
     }
     Fayde.RegisterType(ContentControl, "Fayde.Controls", Fayde.XMLNS);
 }
