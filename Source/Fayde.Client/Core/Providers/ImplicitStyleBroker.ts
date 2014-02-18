077d389d39d55be6bf3b0d732216d320e54af633
BSick7 <brad.sickles@gmail.com>
2014-02-18 12:29:27 -0500

    Moved style swapping so LocalStyle and ImplicitStyle brokers could utilize same code.

--- Source/Fayde.Client/Core/Providers/ImplicitStyleBroker.ts
+++ Source/Fayde.Client/Core/Providers/ImplicitStyleBroker.ts
@@ -92,51 +92,7 @@ module Fayde.Providers {
             if (!isChanged)
                 return;
 
-            var arr = (<IPropertyStorageOwner>fe)._PropertyStorage;
-
-            var oldWalker = MultipleStylesWalker(oldStyles);
-            var newWalker = MultipleStylesWalker(styles);
-            var oldSetter = oldWalker.Step();
-            var newSetter = newWalker.Step();
-
-            var storage: IPropertyStorage;
-            var value: any;
-            var propd: DependencyProperty;
-            while (oldSetter || newSetter) {
-                if (oldSetter && newSetter) {
-                    switch (Setter.Compare(oldSetter, newSetter)) {
-                        case 0:
-                            value = newSetter.ConvertedValue;
-                            propd = newSetter.Property;
-                            oldSetter = oldWalker.Step();
-                            newSetter = newWalker.Step();
-                            break;
-                        case -1:
-                            value = undefined;
-                            propd = oldSetter.Property;
-                            oldSetter = oldWalker.Step();
-                            break;
-                        case 1:
-                            value = newSetter.ConvertedValue;
-                            propd = newSetter.Property;
-                            newSetter = newWalker.Step();
-                            break;
-                    }
-                } else if (newSetter) {
-                    value = newSetter.ConvertedValue;
-                    propd = newSetter.Property;
-                    newSetter = newWalker.Step();
-                } else /*if (oldSetter)*/ {
-                    value = undefined;
-                    propd = oldSetter.Property;
-                    oldSetter = oldWalker.Step();
-                }
-
-                storage = arr[propd._ID];
-                if (!storage)
-                    storage = arr[propd._ID] = propd.Store.CreateStorage(fe, propd);
-                propd.Store.SetImplicitStyle(storage, value);
-            }
+            SwapStyles(fe, MultipleStylesWalker(oldStyles), MultipleStylesWalker(styles));
 
             holder._ImplicitStyles = styles;
             holder._StyleMask = mask;
--- Source/Fayde.Client/Core/Providers/LocalStyleBroker.ts
+++ Source/Fayde.Client/Core/Providers/LocalStyleBroker.ts
@@ -7,41 +7,8 @@ module Fayde.Providers {
     export class LocalStyleBroker {
         static Set(fe: FrameworkElement, newStyle: Style) {
             var holder = <IStyleHolder>fe.XamlNode;
-            var arr = (<IPropertyStorageOwner>fe)._PropertyStorage;
-
-            var oldWalker = SingleStyleWalker(holder._LocalStyle);
-            var newWalker = SingleStyleWalker(newStyle);
             newStyle.Seal();
-            var oldSetter = oldWalker.Step();
-            var newSetter = newWalker.Step();
-            var oldProp: DependencyProperty;
-            var newProp: DependencyProperty;
-            
-            var storage: IPropertyStorage;
-            var oldValue = undefined;
-            var newValue = undefined;
-            var propd: DependencyProperty;
-            while (oldSetter || newSetter) {
-                if (oldSetter) {
-                    propd = oldProp = oldSetter.Property;
-                    oldValue = oldSetter.ConvertedValue;
-                }
-                if (newSetter) {
-                    propd = newProp = newSetter.Property;
-                    newValue = newSetter.ConvertedValue;
-                }
-
-                storage = arr[propd._ID];
-                if (!storage)
-                    storage = arr[propd._ID] = propd.Store.CreateStorage(fe, propd);
-                propd.Store.SetLocalStyleValue(storage, newValue);
-
-                if (oldProp)
-                    oldSetter = oldWalker.Step();
-                if (newProp)
-                    newSetter = newWalker.Step();
-            }
-
+            SwapStyles(fe, SingleStyleWalker(holder._LocalStyle), SingleStyleWalker(newStyle));
             holder._LocalStyle = newStyle;
         }
     }
