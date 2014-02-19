3f42d0b1af4de64ecbbc59e9ed5d5198b97dc1f7
BSick7 <brad.sickles@gmail.com>
2014-02-19 13:26:19 -0500

    Updated fayde controls package to safely add script tag to default.html.

--- Packages/Nuget/Fayde.Controls/tools/install.ps1
+++ Packages/Nuget/Fayde.Controls/tools/install.ps1
@@ -1,23 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
-function Add-ScriptBlock($path, $id, $content) {
-    [xml]$xd = New-Object System.Xml.XmlDocument;
-    $xd.PreserveWhitespace = $true;
-    $xd.Load($path);
-    $defns = $xd.DocumentElement.NamespaceURI;
-    
-    $prevsibling = $xd.html.head.script | ? { [string]::IsNullOrEmpty($_.'data-config') -ne $true -or [string]::IsNullOrEmpty($_.'data-lib') -ne $true } | select -Last 1
-
-    $newblock = $xd.CreateElement("script", $defns);
-    $newblock.SetAttribute("data-lib", $id)
-    $newblock.AppendChild($xd.CreateTextNode($content));
-    $prevsibling.ParentNode.InsertAfter($newblock, $prevsibling);
-    
-    $nl = $xd.CreateSignificantWhitespace([Environment]::NewLine);
-    $prevsibling.ParentNode.InsertAfter($nl, $prevsibling);
-    
-    $xd.Save($path);
-}
+Import-Module (Join-Path $toolsPath "commands.psm1")
 
 $sc = @"
     
@@ -30,4 +13,5 @@ $sc = @"
 $p = [System.IO.Path]::GetDirectoryName($project.FileName)
 $p = Join-Path $p "default.html"
 
+Remove-ScriptBlock $p "Fayde.Controls"
 Add-ScriptBlock $p "Fayde.Controls" $sc
--- Packages/Nuget/Fayde.Controls/tools/uninstall.ps1
+++ Packages/Nuget/Fayde.Controls/tools/uninstall.ps1
@@ -1,23 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
-function Remove-ScriptBlock($path, $id) {
-    [xml]$xd = New-Object System.Xml.XmlDocument;
-    $xd.PreserveWhitespace = $true;
-    $xd.Load($path);
-    $defns = $xd.DocumentElement.NamespaceURI;
-    
-    $head = $xd.html.head;
-
-    $head.script | ? { $_.'data-lib' -eq $id } | foreach { Remove-BlockAndWs $_ }
-
-    $xd.Save($path);
-}
-function Remove-BlockAndWs($block) {
-    $parent = $block.ParentNode;
-    $ws = $block.PreviousSibling;
-    $parent.RemoveChild($block);
-    $parent.RemoveChild($ws);
-}
+Import-Module (Join-Path $toolsPath "commands.psm1")
 
 $p = [System.IO.Path]::GetDirectoryName($project.FileName)
 $p = Join-Path $p "default.html"
