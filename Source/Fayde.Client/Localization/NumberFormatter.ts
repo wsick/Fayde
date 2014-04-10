11b38c83e021108d513d43b90ec0e1907f12f574
BSick7 <brad.sickles@gmail.com>
2014-04-10 14:47:01 -0400

    Implemented standard number formatting.

--- Source/Fayde.Client/Localization/NumberFormatter.ts
+++ Source/Fayde.Client/Localization/NumberFormatter.ts
@@ -29,47 +29,50 @@ module Fayde.Localization {
         var lowerch = ch.toLowerCase();
         if (lowerch < "a" || lowerch > "z")
             return undefined;
-        var num = parseInt(format.substr(1));
-        if (isNaN(num))
-            return undefined;
-
+        var prec: number = null;
+        if (format.length > 1) {
+            var prec = parseInt(format.substr(1));
+            if (isNaN(prec))
+                return undefined;
+        }
+        
         var f = standardFormatters[ch] || standardFormatters[lowerch];
         if (!f)
             return undefined;
-        return f(obj, num);
+        return f(obj, prec);
     }
     interface IStandardFormatter {
-        (obj: number, size: number): string;
+        (obj: number, precision: number): string;
     }
     var standardFormatters: IStandardFormatter[] = [];
-    standardFormatters["c"] = function (obj: number, size: number): string {
-        return obj.toString();
+    standardFormatters["c"] = function (obj: number, precision: number): string {
+        return NumberFormatInfo.Instance.FormatCurrency(obj, precision);
     };
-    standardFormatters["d"] = function (obj: number, size: number): string {
-        return obj.toString();
+    standardFormatters["d"] = function (obj: number, precision: number): string {
+        return NumberFormatInfo.Instance.FormatDecimal(obj, precision);
     };
-    standardFormatters["E"] = function (obj: number, size: number): string {
-        return obj.toString();
+    standardFormatters["E"] = function (obj: number, precision: number): string {
+        return NumberFormatInfo.Instance.FormatExponential(obj, precision).toUpperCase();
     };
-    standardFormatters["e"] = function (obj: number, size: number): string {
-        return obj.toString();
+    standardFormatters["e"] = function (obj: number, precision: number): string {
+        return NumberFormatInfo.Instance.FormatExponential(obj, precision);
     };
-    standardFormatters["f"] = function (obj: number, size: number): string {
-        return obj.toString();
+    standardFormatters["f"] = function (obj: number, precision: number): string {
+        return NumberFormatInfo.Instance.FormatNumber(obj, precision, true);
     };
-    standardFormatters["g"] = function (obj: number, size: number): string {
-        return obj.toString();
+    standardFormatters["g"] = function (obj: number, precision: number): string {
+        return NumberFormatInfo.Instance.FormatGeneral(obj, precision);
     };
-    standardFormatters["n"] = function (obj: number, size: number): string {
-        return obj.toString();
+    standardFormatters["n"] = function (obj: number, precision: number): string {
+        return NumberFormatInfo.Instance.FormatNumber(obj, precision);
     };
-    standardFormatters["p"] = function (obj: number, size: number): string {
-        return obj.toString();
+    standardFormatters["p"] = function (obj: number, precision: number): string {
+        return NumberFormatInfo.Instance.FormatPercent(obj, precision);
     };
-    standardFormatters["X"] = function (obj: number, size: number): string {
-        return obj.toString(16);
+    standardFormatters["X"] = function (obj: number, precision: number): string {
+        return NumberFormatInfo.Instance.FormatHexadecimal(obj, precision).toUpperCase();
     };
-    standardFormatters["x"] = function (obj: number, size: number): string {
-        return obj.toString(16);
+    standardFormatters["x"] = function (obj: number, precision: number): string {
+        return NumberFormatInfo.Instance.FormatHexadecimal(obj, precision);
     };
 }
