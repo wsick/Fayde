b36c3f819b2c1583d2b54d2e1cf3280601c1987f
BSick7 <brad.sickles@gmail.com>
2014-05-17 09:34:57 -0400

    Added a test to see how UpdateSourceTrigger works.

---
+++ Source/WickedSick.LitmusTests/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace WickedSick.LitmusTests
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _SomeText;
+        public string SomeText
+        {
+            get { return this._SomeText; }
+            set
+            {
+                this._SomeText = value;
+                this.PropertyChanged(this, new PropertyChangedEventArgs("SomeText"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
