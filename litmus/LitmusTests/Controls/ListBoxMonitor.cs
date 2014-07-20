58699e6d6502422c5b4b9795cebe1411f15672bc
BSick7 <brad.sickles@gmail.com>
2014-07-20 11:05:41 -0400

    Moved litmus tests into separate project.

---
+++ litmus/LitmusTests/Controls/ListBoxMonitor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace LitmusTests.Controls
+{
+    public class ListBoxMonitor : ListBox
+    {
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            Debug.WriteLine("MeasureOverride (ListBox): " + availableSize.ToString());
+            return base.MeasureOverride(availableSize);
+        }
+    }
+}
