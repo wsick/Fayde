e5459e4ca4351d1fa91c6c21884a7b3bd7a352f6
BSick7 <brad.sickles@gmail.com>
2014-10-09 12:13:40 -0400

    Fixing attached ui reactions.

--- src/Core/UIReactionAttached.ts
+++ src/Core/UIReactionAttached.ts
@@ -1,11 +1,14 @@
 module Fayde {
     export function UIReactionAttached<TValue>(propd: DependencyProperty, callback?: IUIReactionCallback<TValue>) {
-        var aname = GetTypeName(propd.OwnerType) + '.' + propd.Name;
+        propd.ChangedCallback = reaction<TValue>(GetTypeName(propd.OwnerType) + '.' + propd.Name, callback);
+    }
+
+    function reaction<T>(name: string, callback?: IUIReactionCallback<T>) {
         return (uie: UIElement, args: DependencyPropertyChangedEventArgs) => {
             var ov = args.OldValue;
             var nv = args.NewValue;
             var upd = uie.XamlNode.LayoutUpdater;
-            upd.setAttachedValue(aname, nv);
+            upd.setAttachedValue(name, nv);
             callback && callback(upd, ov, nv, uie);
         };
     }
