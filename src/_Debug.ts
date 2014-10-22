2e1be2b26db53c8c06f49bca6eb624ae700cbf23
BSick7 <brad.sickles@gmail.com>
2014-10-22 15:37:11 -0400

    Added back layer stringification for debug.

--- src/_Debug.ts
+++ src/_Debug.ts
@@ -26,4 +26,57 @@ module Fayde {
 
         return obj;
     }
+
+    export function debugLayersRaw (): string {
+        var app = Fayde.Application.Current;
+        var output = "";
+        for (var walker = app.MainSurface.walkLayers(); walker.step();) {
+            output += stringify(walker.current);
+        }
+        return output;
+    }
+
+    function stringify (updater: minerva.core.Updater, level: number = 0): string {
+        var node = updater.getAttachedValue("$node");
+        var xobj = node.XObject;
+
+        var output = "";
+
+        for (var i = 0; i < level; i++) {
+            output += "\t";
+        }
+
+        output += xobj.constructor.name;
+        output += "[" + xobj._ID + "]";
+
+        var ns = node.NameScope;
+        var nsr = !ns ? "^" : (ns.IsRoot ? "+" : "-");
+        output += " [" + nsr + node.Name + "]";
+
+        output += "\n";
+
+        for (var walker = updater.tree.walk(); walker.step();) {
+            output += stringify(walker.current, level + 1);
+        }
+
+        return output;
+    }
+
+    export function getById (id: number) {
+        var app = Fayde.Application.Current;
+        for (var walker = app.MainSurface.walkLayers(); walker.step();) {
+            for (var subwalker = walker.current.walkDeep(); subwalker.step();) {
+                var upd = subwalker.current;
+                var node = upd.getAttachedValue("$node");
+                var xobj = node.XObject;
+                if (xobj._ID === id) {
+                    return {
+                        obj: xobj,
+                        node: node,
+                        updater: upd
+                    };
+                }
+            }
+        }
+    }
 }
