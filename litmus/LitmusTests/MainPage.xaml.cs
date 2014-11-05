81774ffa1378ab8727aa1c87616fec9c67c5eca5
BSick7 <brad.sickles@gmail.com>
2014-11-05 09:56:57 -0500

    Added testing of layout slot.

--- litmus/LitmusTests/MainPage.xaml.cs
+++ litmus/LitmusTests/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -17,6 +18,64 @@ namespace LitmusTests
         public MainPage()
         {
             InitializeComponent();
+            Loaded += MainPage_Loaded;
+            LayoutUpdated += MainPage_LayoutUpdated;
+        }
+
+        void MainPage_LayoutUpdated(object sender, EventArgs e)
+        {
+            var node = new FENode(this);
+            FindElsWithLayoutClip();
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void FindElsWithLayoutClip()
+        {
+            foreach (var fe in FENode.Flatten(new FENode(this)))
+            {
+                var lc = LayoutInformation.GetLayoutClip(fe);
+                var ls = LayoutInformation.GetLayoutSlot(fe);
+            }
+        }
+    }
+
+    public class FENode
+    {
+        public FrameworkElement _Element { get; set; }
+
+        public FENode(FrameworkElement element)
+        {
+            _Element = element;
+        }
+
+        public List<FENode> Children
+        {
+            get
+            {
+                var list = new List<FENode>();
+                for (var i = 0; i < VisualTreeHelper.GetChildrenCount(this._Element); i++)
+                {
+                    var child = (FrameworkElement)VisualTreeHelper.GetChild(this._Element, i);
+                    list.Add(new FENode(child));
+                }
+                return list;
+            }
+        }
+
+        public static IEnumerable<FrameworkElement> Flatten(FENode node)
+        {
+            yield return node._Element;
+            foreach (var child in node.Children)
+            {
+                foreach (var granchild in Flatten(child))
+                {
+                    yield return granchild;
+                }
+            }
         }
     }
 }
