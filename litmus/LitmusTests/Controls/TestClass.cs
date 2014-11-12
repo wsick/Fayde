7002ed3be86ae9d09c3d59ffece4e41755ba735f
BSick7 <brad.sickles@gmail.com>
2014-11-12 09:56:21 -0500

    Litmus testing content control.  Improving tooltip test.

---
+++ litmus/LitmusTests/Controls/TestClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace LitmusTests.Controls
+{
+    public class TestClass
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public TestClass()
+        {
+            Name = "Brad";
+            Age = 28;
+        }
+    }
+}
