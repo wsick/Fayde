cb96761a02a96537266695e202dd6a3d982c2ae2
BSick7 <brad.sickles@gmail.com>
2014-12-10 09:27:21 -0500

    Fixed getter only `pixelWidth`/`pixelHeight`.

--- src/Media/Imaging/ImageSource.ts
+++ src/Media/Imaging/ImageSource.ts
@@ -2,11 +2,23 @@
 
 module Fayde.Media.Imaging {
     export class ImageSource extends DependencyObject implements minerva.controls.image.IImageSource {
-        pixelWidth: number = 0;
-        pixelHeight: number = 0;
-        lock() { }
-        unlock() { }
-        get image(): HTMLImageElement { return undefined; }
+        get pixelWidth (): number {
+            return 0;
+        }
+
+        get pixelHeight (): number {
+            return 0;
+        }
+
+        lock () {
+        }
+
+        unlock () {
+        }
+
+        get image (): HTMLImageElement {
+            return undefined;
+        }
     }
     Fayde.CoreLibrary.add(ImageSource);
 }
