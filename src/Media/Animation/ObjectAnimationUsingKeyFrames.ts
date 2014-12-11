afee48aa0846718e215f5bedc645bb214ee54d30
BSick7 <brad.sickles@gmail.com>
2014-12-10 19:23:18 -0500

    Isolating key frame value conversion.

--- src/Media/Animation/ObjectAnimationUsingKeyFrames.ts
+++ src/Media/Animation/ObjectAnimationUsingKeyFrames.ts
@@ -2,26 +2,32 @@
 
 module Fayde.Media.Animation {
     export class ObjectAnimationUsingKeyFrames extends AnimationUsingKeyFrames {
-        Resolve(target: DependencyObject, propd: DependencyProperty): boolean {
-            var enumerator = this.KeyFrames.getEnumerator();
-            while (enumerator.moveNext()) {
-                var keyFrame = <ObjectKeyFrame>enumerator.current;
+        Resolve (target: DependencyObject, propd: DependencyProperty): boolean {
+            for (var en = this.KeyFrames.getEnumerator(); en.moveNext();) {
+                var keyFrame = <ObjectKeyFrame>en.current;
                 var value = keyFrame.Value;
                 if (value == null) {
                     keyFrame.ConvertedValue = undefined;
                 } else {
-                    var converted: any;
-                    try {
-                        converted = nullstone.convertAnyToType(value, <Function>propd.GetTargetType());
-                    } catch (err) {
-                        console.warn("Error resolving ObjectAnimation Value.");
+                    var cv = convertKeyFrame(propd, value);
+                    if (cv === BAD_CONVERSION)
                         return false;
-                    }
-                    keyFrame.ConvertedValue = converted;
+                    keyFrame.ConvertedValue = cv;
                 }
             }
             return super.Resolve(target, propd);
         }
     }
     Fayde.CoreLibrary.add(ObjectAnimationUsingKeyFrames);
+
+    var BAD_CONVERSION = {};
+
+    function convertKeyFrame (propd: DependencyProperty, value: any): any {
+        try {
+            return nullstone.convertAnyToType(value, <Function>propd.GetTargetType());
+        } catch (err) {
+            console.warn("Error resolving ObjectAnimation Value.");
+            return BAD_CONVERSION;
+        }
+    }
 }
