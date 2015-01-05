5a87e50a5c75ef8ee424ff121ae6d4421d1bdfe6
BSick7 <brad.sickles@gmail.com>
2015-01-05 18:11:35 -0500

    Adding unify.json.

--- dist/Fayde.js
+++ dist/Fayde.js
@@ -1,6 +1,6 @@
 var Fayde;
 (function (Fayde) {
-    Fayde.Version = '0.16.6';
+    Fayde.Version = '0.16.7';
 })(Fayde || (Fayde = {}));
 if (!Array.isArray) {
     Array.isArray = function (arg) {
