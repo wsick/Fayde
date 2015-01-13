f059ef497d7adb8ebe4c2da9e28d4e4667d086a9
BSick7 <brad.sickles@gmail.com>
2015-01-13 15:57:51 -0500

    Updating version without Themes dir.

--- dist/fayde.js
+++ dist/fayde.js
@@ -1,6 +1,6 @@
 var Fayde;
 (function (Fayde) {
-    Fayde.Version = '0.16.9';
+    Fayde.Version = '0.16.10';
 })(Fayde || (Fayde = {}));
 if (!Array.isArray) {
     Array.isArray = function (arg) {
