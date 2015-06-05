269b8e934f188273308f6eacfa2f5a8ec0874772
BSick7 <brad.sickles@gmail.com>
2015-06-05 17:22:17 -0400

    Upping minerva to 0.4.12.  Closes issue #109.

--- dist/fayde.js
+++ dist/fayde.js
@@ -1,6 +1,6 @@
 var Fayde;
 (function (Fayde) {
-    Fayde.Version = '0.16.29';
+    Fayde.Version = '0.16.30';
 })(Fayde || (Fayde = {}));
 if (!Array.isArray) {
     Array.isArray = function (arg) {
