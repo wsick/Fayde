f9d5999e03d58a27f6dc4f12315b9c521b223ff5
BSick7 <brad.sickles@gmail.com>
2015-06-05 10:17:52 -0400

    Updating fayde with minerva fixed build.

--- dist/fayde.js
+++ dist/fayde.js
@@ -1,6 +1,6 @@
 var Fayde;
 (function (Fayde) {
-    Fayde.Version = '0.16.28';
+    Fayde.Version = '0.16.29';
 })(Fayde || (Fayde = {}));
 if (!Array.isArray) {
     Array.isArray = function (arg) {
