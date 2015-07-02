5b8cae9ea05bbd277ce7d9f6a7e8d84d8973f68d
BSick7 <brad.sickles@gmail.com>
2015-07-02 08:07:47 -0400

    Implementing HyperlinkButton.TargetName switching.

--- src/Controls/HyperlinkButton.ts
+++ src/Controls/HyperlinkButton.ts
@@ -7,58 +7,78 @@ module Fayde.Controls {
         NavigateUri: Uri;
         TargetName: string;
 
-        constructor() {
+        constructor () {
             super();
             this.DefaultStyleKey = HyperlinkButton;
         }
 
-        OnApplyTemplate() {
+        OnApplyTemplate () {
             super.OnApplyTemplate();
             this.UpdateVisualState(false);
         }
 
-        OnClick() {
+        OnClick () {
             super.OnClick();
             if (this.NavigateUri != null)
                 this._Navigate();
         }
 
         /*
-        private _GetAbsoluteUri(): Uri {
-            var destination = this.NavigateUri;
-            if (!destination.IsAbsoluteUri) {
-                var original = destination.OriginalString;
-                if (original && original.charAt(0) !== '/')
-                    throw new NotSupportedException();
-                destination = new Uri(App.Current.GetHost().GetSource(), destination);
-            }
-            return destination;
-        }
-        */
-        private _Navigate() {
+         private _GetAbsoluteUri(): Uri {
+         var destination = this.NavigateUri;
+         if (!destination.IsAbsoluteUri) {
+         var original = destination.OriginalString;
+         if (original && original.charAt(0) !== '/')
+         throw new NotSupportedException();
+         destination = new Uri(App.Current.GetHost().GetSource(), destination);
+         }
+         return destination;
+         }
+         */
+
+        private _Navigate () {
             var targetName = this.TargetName;
-            var targetUie;
-            if (!targetName) {
-                targetUie = VisualTreeHelper.GetParentOfType(this, Fayde.Controls.Frame);
-            } else {
-                targetUie = this.FindName(targetName, true);
+            switch ((targetName || "").toLowerCase()) {
+                case "_blank":
+                case "_media":
+                case "_search":
+                    launchDummyLink(targetName.toLowerCase(), this.NavigateUri);
+                    return;
+                case "_parent":
+                case "_self":
+                case "_top":
+                default:
+                    break;
             }
 
-            if (!targetUie) {
-                window.location.href = this.NavigateUri.toString();
-            } else if (targetUie instanceof Frame) {
+            var targetUie = this._FindTargetElement();
+            if (targetUie instanceof Frame)
                 window.location.hash = this.NavigateUri.toString();
-            } else {
-                window.open(this.NavigateUri.toString(), targetName);
-            }
+            else
+                window.location.href = this.NavigateUri.toString();
+        }
+
+        private _FindTargetElement () {
+            var targetName = this.TargetName;
+            if (!targetName)
+                return VisualTreeHelper.GetParentOfType(this, Fayde.Controls.Frame);
+            return this.FindName(targetName, true);
         }
     }
     Fayde.CoreLibrary.add(HyperlinkButton);
-    TemplateVisualStates(HyperlinkButton, 
-        { GroupName: "CommonStates", Name: "Normal" },
-        { GroupName: "CommonStates", Name: "MouseOver" },
-        { GroupName: "CommonStates", Name: "Pressed" },
-        { GroupName: "CommonStates", Name: "Disabled" },
-        { GroupName: "FocusStates", Name: "Unfocused" },
-        { GroupName: "FocusStates", Name: "Focused" });
+    TemplateVisualStates(HyperlinkButton,
+        {GroupName: "CommonStates", Name: "Normal"},
+        {GroupName: "CommonStates", Name: "MouseOver"},
+        {GroupName: "CommonStates", Name: "Pressed"},
+        {GroupName: "CommonStates", Name: "Disabled"},
+        {GroupName: "FocusStates", Name: "Unfocused"},
+        {GroupName: "FocusStates", Name: "Focused"});
+
+    var dummyLink: HTMLAnchorElement;
+    function launchDummyLink (target: string, navigateUri: Uri) {
+        dummyLink = dummyLink || document.createElement('a');
+        dummyLink.href = navigateUri.toString();
+        dummyLink.target = target;
+        dummyLink.click();
+    }
 }
