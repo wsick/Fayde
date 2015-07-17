c90be7f02ede2b335a96b2a0e4c0ff2f4898c65f
BSick7 <brad.sickles@gmail.com>
2015-07-17 18:10:08 -0400

    Updating version.

--- dist/fayde.js
+++ dist/fayde.js
@@ -1,6 +1,6 @@
 var Fayde;
 (function (Fayde) {
-    Fayde.Version = '0.16.51';
+    Fayde.Version = '0.16.52';
 })(Fayde || (Fayde = {}));
 if (!Array.isArray) {
     Array.isArray = function (arg) {
