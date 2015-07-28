ca5e770dc27feb00605cdd2c4c76c8c9c653e3c0
BSick7 <brad.sickles@gmail.com>
2015-07-28 13:09:43 -0400

    Adding png test.

--- testsite/Media/ImageSetSourceViewModel.ts
+++ testsite/Media/ImageSetSourceViewModel.ts
@@ -1,5 +1,6 @@
 class ImageSetSourceViewModel extends Fayde.MVVM.ViewModelBase {
     SomeBuffer: ArrayBuffer = null;
+    AnotherBuffer: ArrayBuffer = null;
 
     constructor () {
         super();
@@ -13,7 +14,14 @@ class ImageSetSourceViewModel extends Fayde.MVVM.ViewModelBase {
         req.responseType = 'arraybuffer';
         req.onload = () => this.SomeBuffer = req.response;
         req.send(null);
+
+        var req2 = new XMLHttpRequest();
+        req2.overrideMimeType('text/plain; charset=x-user-defined');
+        req2.open('GET', 'Images/twitter.png', true);
+        req2.responseType = 'arraybuffer';
+        req2.onload = () => this.AnotherBuffer = req2.response;
+        req2.send(null);
     }
 }
-Fayde.MVVM.NotifyProperties(ImageSetSourceViewModel, ["SomeBuffer"]);
+Fayde.MVVM.NotifyProperties(ImageSetSourceViewModel, ["SomeBuffer", "AnotherBuffer"]);
 export = ImageSetSourceViewModel;
