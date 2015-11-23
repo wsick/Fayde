31b9352d87b7b1c311ee8666bf9566971a8e8d7e
Brad Sickles <brad.sickles@gmail.com>
2015-11-23 15:10:07 -0500

    Implementing AddMonths and GetDayOfWeek.

--- src/Localization/Calendar.ts
+++ src/Localization/Calendar.ts
@@ -4,6 +4,7 @@ module Fayde.Localization {
     export abstract class Calendar {
         ID: number = 1;
         Eras: number[] = [1];
+        CurrentEra = 1;
         EraNames: string[] = ["A.D."];
         CurrentEraValue: number = 1;
         TwoDigitYearMax: number = 2029;
@@ -11,9 +12,18 @@ module Fayde.Localization {
         MinSupportedDateTime = new DateTime(1, 1, 1, 0, 0, 0, 0);
 
         abstract GetDaysInMonth(year: number, month: number, era: number): number;
-        abstract AddDays(): DateTime;
-        abstract AddMonths(): DateTime;
-        abstract GetDayOfWeek(): DayOfWeek;
+        abstract GetDayOfWeek(time: DateTime): DayOfWeek;
         abstract IsLeapYear(year: number, era?: number): boolean;
+
+        static CheckAddResult(ticks: number, minValue: DateTime, maxValue: DateTime) {
+            if (ticks < minValue.Ticks || ticks > maxValue.Ticks)
+                throw new ArgumentException("Invalid add result (out of range)");
+        }
+
+        abstract AddMonths(time: DateTime, months: number): DateTime;
+
+        AddWeeks(time: DateTime, weeks: number): DateTime {
+            return time.AddDays(weeks * 7);
+        }
     }
 }
