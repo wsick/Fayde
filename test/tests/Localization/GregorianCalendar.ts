f6174a2f93c895d20da7766d2381c3619bde6515
Blacksun <angel.calvasp@outlook.com>
2016-04-17 22:17:05 -0500

    Asserted AddMonths
    Small change

--- test/tests/Localization/GregorianCalendar.ts
+++ test/tests/Localization/GregorianCalendar.ts
@@ -14,27 +14,12 @@ export function load() {
         }, undefined, "Should throw when getting days of invalid year/month or era.");
         
         
-        try {
-            var days = calendar.AddMonths(date,2);
-            
-        } catch (err) {
-            ok(false, "adding months should not error. " + err.toString());
-        }
+        var days = calendar.AddMonths(date,2);
         
-        try {
-            var dayOfWeek = calendar.GetDayOfWeek(date);
-            
-        } catch (err) {
-            ok(false, "get day of week should not error. " + err.toString());
-        }
+        var dayOfWeek = calendar.GetDayOfWeek(date);
         
-        try {
-            var isLeapYear = calendar.IsLeapYear(1990);
+        var isLeapYear = calendar.IsLeapYear(1990);
             isLeapYear = calendar.IsLeapYear(1990,1);
-            
-        } catch (err) {
-            ok(false, "is leap year should not error. " + err.toString());
-        }
         
         
         
